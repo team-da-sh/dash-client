8993532c18dc4111bbecb12b398cd4bdd617d9b3
constantly-dev <rntqhdl10@naver.com>
2025-01-07 04:22:47 +0900

    refactor: query provider app으로 이동 (#1)

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import queryClient from './queryClient.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>
 );
