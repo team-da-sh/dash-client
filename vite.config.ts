fa8d7c5ac91fe0943c8a099a9de89c7223d28dbc
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-01-07 20:19:06 +0900

    setting: vanila extract 설치(#3)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    vanillaExtractPlugin({
+      // vanilla-extract plugin 추가
+      identifiers: "debug",
+    }),
+  ],
+});
