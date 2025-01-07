a12ec5958ff5f21c3e77b5fd42fdd831be896a7d
Heesun Park <phs1585@naver.com>
2025-01-07 14:33:13 +0900

    setting: route 설정정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-
+import App from '@/App.tsx';
+import { router } from '@/routes/Router.tsx';
+import { RouterProvider } from 'react-router-dom';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
