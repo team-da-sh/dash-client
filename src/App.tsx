1f04e3ad956bea8e80d43187ae2c34e8e7a8a9ab
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-01-07 20:40:25 +0900

    setting: global, reset 파일 및 모바일뷰 대응 설정(#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-function App() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-}
+import { useEffect } from "react";
+import { rootStyle } from "./styles/global.css";
+import "./styles/index.css";
+
+const App = () => {
+  const setScreenSize = () => {
+    // vh 관련
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // window width 관련
+    const windowWidth =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    const maxWidth = Math.min(375, windowWidth);
+    document.documentElement.style.setProperty(
+      "--app-max-width",
+      `${maxWidth}px`
+    );
+  };
+
+  useEffect(() => {
+    setScreenSize();
+    window.addEventListener("resize", setScreenSize);
+
+    return () => {
+      window.removeEventListener("resize", setScreenSize);
+    };
+  }, []);
+  return <div className={rootStyle}></div>;
+};
 
 export default App;
---
+++ src/styles/global.css.ts
@@ -0,0 +1,40 @@
+import { globalStyle, style } from "@vanilla-extract/css";
+
+/* CSS Variables */
+globalStyle(":root", {
+  vars: {
+    "--min-width": "375px",
+    "--max-width": "430px",
+  },
+});
+
+/* HTML & Body additional styles */
+globalStyle("html, body", {
+  scrollbarWidth: "none",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  scrollBehavior: "smooth",
+  background: "#7700FF",
+});
+
+/* Scrollbar Hide */
+globalStyle("::-webkit-scrollbar", {
+  display: "none",
+});
+
+/* Root Container */
+export const rootStyle = style({
+  minWidth: "var(--min-width)",
+  maxWidth: "var(--max-width)",
+  width: "100%",
+  minHeight: "100dvh",
+  backgroundColor: "#fff",
+  margin: "0 auto",
+
+  "@media": {
+    "(min-width: 430px)": {
+      boxShadow: "0 0 2px rgba(0, 0, 0, 0.4)",
+    },
+  },
+});
