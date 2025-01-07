08679e89429cb8acd9992688fe8aac4db56fbda3
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-07 18:34:16 +0900

    setting: svgr 설치, 설정 (#7)

---
+++ custom.d.ts
@@ -0,0 +1,8 @@
+declare module '*.svg?react' {
+  import React = require('react');
+  export const ReactComponent: React.ComponentType<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}
