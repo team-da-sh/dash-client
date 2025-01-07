6c765a65b7b1f58f5128530403ef3fc2cde095b6
kimkyuhong <bobo3766@naver.com>
2025-01-07 16:19:31 +0900

    feat: 스토리북 예시용 button 구현 (#5)

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ButtonHTMLAttributes } from 'react';
+import Button from '../components/Button/Button';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'outline';
+  size?: 'xLarge' | 'large' | 'medium';
+  isDisabled?: boolean;
+}
+
+const meta = {
+  title: 'Common/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['primary', 'secondary', 'tertiary', 'outline'],
+    },
+    size: {
+      control: { type: 'radio' },
+      options: ['xLarge', 'large', 'medium'],
+    },
+    children: {
+      control: { type: 'text' },
+    },
+    isDisabled: {
+      control: { type: 'boolean' },
+    },
+  },
+  args: {
+    variant: 'primary',
+    size: 'medium',
+    children: 'Button',
+    isDisabled: false,
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+const createButtonStory = (variant: ButtonProps['variant']) => ({
+  args: {
+    variant,
+  },
+  argsType: {
+    variant: {
+      control: false,
+    },
+  },
+});
+
+export const Primary: Story = createButtonStory('primary');
+
+export const Secondary: Story = createButtonStory('secondary');
+
+export const Tertiary: Story = createButtonStory('tertiary');
+
+export const Outline: Story = createButtonStory('outline');
