bb73047dc23e60d96cf244086e4fe07b6ceff190
heesunee <heesun729@uos.ac.kr>
2025-01-12 17:23:43 +0900

    setting: 경로 상수 작성 (#4)

---
+++ src/routes/routesConfig.ts
@@ -0,0 +1,55 @@
+export const ROUTES_CONFIG = {
+    home: {
+        title: 'Home',
+        path:'/home',
+    },
+    login: {
+        title: 'Login',
+        path:'/login',
+    },
+    search:{
+        title: 'Search',
+        path:'/search',
+    },
+    class:{
+        title: 'Class',
+        path:'/class/:id',
+    },
+    dancer:{
+        title: 'Dancer',
+        path:'/dancer/:id',
+    },
+    reservation: {
+        title: 'Reservation',
+        path:'/reservation',
+    },
+    mypage: {
+        title: 'Mypage',
+        path:'/mypage',
+    },
+    mypageReservation: {
+        title: 'MypageReservation',
+        path:'/mypage/reservation',
+    },
+    mypageReservationDetail: {
+        title: 'MypageReservationDetail',
+        path:'/mypage/reservation/:id',
+    },
+    classRegister: {
+        title: 'ClassRegister',
+        path:'/class-register',
+    },
+    instructorRegister: {
+        title: 'InstructorRegister',
+        path:'/instructor-register',
+    },
+    instructorClassList: { 
+        title: 'instructorClassList', 
+        path: '/instructor/class-list' 
+    },
+    instructorClassDetail: { 
+        title: 'instructorClassDetail', 
+        path: '/instructor/class-detail' 
+    },
+}
+
