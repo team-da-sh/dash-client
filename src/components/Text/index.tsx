36bae14f4180962c4fdbd0d1140305e0900f5274
kimkyuhong <bobo3766@naver.com>
2025-01-13 18:33:39 +0900

    feat: font명 변경된거 적용 (#21)

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,24 +2,10 @@ import { HTMLAttributes } from 'react';
 import { textStyle } from './index.css';
 
 interface TextProps extends HTMLAttributes<HTMLParagraphElement> {
-  tag?:
-    | 'body01_reg'
-    | 'body01_med'
-    | 'body01_med_long'
-    | 'body01_smbold'
-    | 'body01_smbold_long'
-    | 'body02_reg'
-    | 'body02_med'
-    | 'body02_med_narrow'
-    | 'body02_smbold'
-    | 'body02_smbold_narrow'
-    | 'caption01_reg'
-    | 'caption01_reg_narr'
-    | 'caption01_med'
-    | 'caption01_smbold';
+  tag?: 'b1' | 'b2' | 'b3' | 'b4' | 'b5' | 'b6' | 'b7' | 'b8' | 'b9' | 'b10' | 'c1' | 'c2' | 'c3' | 'c4';
 }
 
-const Text = ({ tag = 'body02_med', children, ...props }: TextProps) => {
+const Text = ({ tag = 'b1', children, ...props }: TextProps) => {
   return (
     <p className={textStyle({ tag: tag })} {...props}>
       {children}
