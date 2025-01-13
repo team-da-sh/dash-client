0f485752163ff99b29501459f650e03b6b6ebe42
heesunee <heesun729@uos.ac.kr>
2025-01-13 22:31:09 +0900

    feat: small 높이 옵션 추가 (#25)

--- src/components/Tag/index.css.ts
+++ src/components/Tag/index.css.ts
@@ -1,43 +1,45 @@
-import { style, styleVariants } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 import { vars } from '../../styles/theme.css';
 
-export const tagBase = style({
-  display: 'inline-flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  color: vars.colors.white,
-});
-
-export const tagTypeStyles = styleVariants({
-  genre: {
-    backgroundColor: vars.colors.main03,
+export const tag = recipe({
+  base: {
+    display: 'inline-flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: vars.colors.white,
   },
-  level: {
-    backgroundColor: vars.colors.sub07,
-  },
-  deadline: {
-    backgroundColor: vars.colors.gray10,
-  },
-});
-
-export const tagSizeStyles = styleVariants({
-  small: {
-    borderRadius: '2px',
-    padding: '0.4rem 0.6rem',
-    gap: '1rem',
-    ...vars.fonts.caption01_med,
-  },
-
-  medium: {
-    borderRadius: '2.4px',
-    padding: '0.3rem 0.8rem',
-    gap: '1.2rem',
-    ...vars.fonts.body02_med,
-  },
-
-  thumbnail: {
-    borderRadius: '0px 4px 4px 0px',
-    padding: '0.2rem 0.8rem',
-    gap: '1rem',
+  variants: {
+    type: {
+      genre: {
+        backgroundColor: vars.colors.main03,
+      },
+      level: {
+        backgroundColor: vars.colors.sub07,
+      },
+      deadline: {
+        backgroundColor: vars.colors.gray10,
+      },
+    },
+    size: {
+      small: {
+        height: '1.8rem',
+        borderRadius: '2px',
+        padding: '0.4rem 0.6rem',
+        gap: '1rem',
+        ...vars.fonts.caption01_med,
+      },
+      medium: {
+        borderRadius: '2.4px',
+        padding: '0.3rem 0.8rem',
+        gap: '1.2rem',
+        ...vars.fonts.body02_med,
+      },
+      thumbnail: {
+        borderRadius: '0px 4px 4px 0px',
+        padding: '0.2rem 0.8rem',
+        gap: '1rem',
+        ...vars.fonts.caption01_med,
+      },
+    },
   },
 });
--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithRef } from 'react';
-import * as styles from '@/components/Tag/index.css';
+import { tag } from '@/components/Tag/index.css';
 
 interface TagProps extends ComponentPropsWithRef<'div'> {
   size: 'small' | 'medium' | 'thumbnail';
@@ -8,7 +8,7 @@ interface TagProps extends ComponentPropsWithRef<'div'> {
 
 const Tag = ({ size, type, children, ...props }: TagProps) => {
   return (
-    <div className={`${styles.tagBase} ${styles.tagTypeStyles[type]} ${styles.tagSizeStyles[size]}`} {...props}>
+    <div className={tag({ size, type })} {...props}>
       {children}
     </div>
   );
