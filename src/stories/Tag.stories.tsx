3428ac6d1a8fcba5f4e4f99486297c7c7cca6aca
heesunee <heesun729@uos.ac.kr>
2025-01-13 18:01:26 +0900

    feat: storybook 수정 (#25)

--- src/stories/Tag.stories.tsx
+++ src/stories/Tag.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Tag from '../components/Tag/Tag';
+import Tag from '../components/Tag/index';
 
 const meta = {
   title: 'Common/Tag',
