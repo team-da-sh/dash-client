2385a092136f745e6aad5918db12b367fa90d968
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-14 21:55:53 +0900

    feat: flex generator 추가 (#23)

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -6,30 +6,63 @@ interface FlexProps extends HTMLAttributes<HTMLDivElement> {
   direction?: 'row' | 'column';
   align?: 'flexStart' | 'flexEnd' | 'center' | 'stretch' | 'baseline';
   justify?: 'flexStart' | 'flexEnd' | 'center' | 'spaceBetween' | 'spaceAround' | 'spaceEvenly';
+  gap?: string;
   wrap?: 'nowrap' | 'wrap' | 'wrapReverse';
-  gap?: string | number;
   grow?: 'grow0' | 'grow1';
+  position?: 'static' | 'absolute' | 'relative' | 'fixed' | 'sticky';
+  width?: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
+  marginTop?: string;
+  marginBottom?: string;
 }
 
 const Flex = ({
-  tag: Component = 'div',
+  tag = 'div',
   direction,
   align,
   justify,
-  wrap,
   gap,
+  wrap,
   grow,
+  position,
+  width,
+  height,
+  margin,
+  padding,
+  marginTop,
+  marginBottom,
   children,
   ...props
 }: FlexProps) => {
+  const Element = tag;
+
+  const inlineStyles = {
+    position,
+    gap,
+    width,
+    height,
+    margin,
+    padding,
+    marginTop,
+    marginBottom,
+  };
+
   return (
-    <Component
-      className={flexStyle({ direction, align, justify, wrap, grow })}
-      style={{ gap, ...props.style }}
+    <Element
+      className={flexStyle({
+        direction,
+        align,
+        justify,
+        wrap,
+        grow,
+      })}
+      style={inlineStyles}
       {...props}
     >
       {children}
-    </Component>
+    </Element>
   );
 };
 
