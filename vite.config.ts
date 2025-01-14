ecf5bcc236c10ca0414f505a42baf7ffd3a85387
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-01-14 17:27:24 +0900

    setting: vite-tsconfig-paths 모듈 설치(#18)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 // or "@vitejs/plugin-react" based on your preference
 import svgr from 'vite-plugin-svgr';
-import { tsconfigPaths } from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
