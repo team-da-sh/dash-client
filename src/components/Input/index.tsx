a5d9041e2a72c37e9d84f28e2ac3aac7f8f52687
constantly-dev <rntqhdl10@naver.com>
2025-01-14 17:39:36 +0900

    feat: input ref 지정 (#35)

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react';
+import { ForwardedRef, forwardRef, InputHTMLAttributes } from 'react';
 import * as style from './index.css';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -6,13 +6,13 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   helperText?: string;
 }
 
-const Input = ({ isError, helperText }: InputProps) => {
+const Input = ({ isError, helperText, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
   return (
     <div className={style.containerStyle}>
-      <input type="text" className={style.inputStyle({ isError })} placeholder="예시를 써주세요" />
+      <input ref={ref} type="text" className={style.inputStyle({ isError })} placeholder="예시를 써주세요" {...props} />
       {helperText && <p className={style.helperTextStyle({ isError })}>{helperText}</p>}
     </div>
   );
 };
 
-export default Input;
+export default forwardRef(Input);
