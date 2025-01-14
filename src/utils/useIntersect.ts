65d5d16e1b245526c4dbe149f879599f3a6217cd
kimkyuhong <bobo3766@naver.com>
2025-01-15 00:25:19 +0900

    feat: useIntersect 훅 구현

--- src/utils/useIntersect.ts
+++ src/utils/useIntersect.ts
@@ -1,24 +1,25 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
 
-type IntersectHandler = (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void;
-
-export const useIntersect = (onIntersect: IntersectHandler, options?: IntersectionObserverInit) => {
+export const useIntersect = (initialVisible: boolean, options?: IntersectionObserverInit) => {
   const ref = useRef<HTMLDivElement>(null);
-  const callback = useCallback(
-    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) onIntersect(entry, observer);
-      });
-    },
-    [onIntersect]
-  );
+
+  const [isVisible, setIsVisible] = useState(initialVisible);
+
+  const callback = useCallback((entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      setIsVisible(!entry.isIntersecting);
+    });
+  }, []);
 
   useEffect(() => {
     if (!ref.current) return;
-    const observer = new IntersectionObserver(callback, options);
+
+    // 이미지 영역에서 헤더 높이를 제외한 비율값
+    const threshold = 1 - (ref.current?.offsetHeight - 60) / ref.current?.offsetHeight;
+    const observer = new IntersectionObserver(callback, { ...options, threshold: threshold });
     observer.observe(ref.current);
     return () => observer.disconnect();
   }, [ref, options, callback]);
 
-  return ref;
+  return [ref, isVisible] as [MutableRefObject<HTMLDivElement | null>, boolean];
 };
