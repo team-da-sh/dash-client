3c8825d6d0dfb87573386300cd78ac791c37f7fe
kimkyuhong <bobo3766@naver.com>
2025-01-14 16:45:34 +0900

    code review: 코드리뷰 반영 (#22)

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -1,5 +1,6 @@
+import { headStyle } from '@/components/Head/index.css';
 import { ComponentPropsWithoutRef } from 'react';
-import { headStyle } from './index.css';
+
 
 type HeadLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 type HeadTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'h7';
