a220c4d0144b954f467a0eccf230642785c21c06
kimkyuhong <bobo3766@naver.com>
2025-01-14 16:40:28 +0900

    code review: 절대경로 및 최적화 (#21)

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,5 +1,5 @@
 import { HTMLAttributes } from 'react';
-import { textStyle } from './index.css';
+import { textStyle } from '@/components/Text/index.css';
 
 interface TextProps extends HTMLAttributes<HTMLParagraphElement> {
   tag?: 'b1' | 'b2' | 'b3' | 'b4' | 'b5' | 'b6' | 'b7' | 'b8' | 'b9' | 'b10' | 'c1' | 'c2' | 'c3' | 'c4';
@@ -7,7 +7,7 @@ interface TextProps extends HTMLAttributes<HTMLParagraphElement> {
 
 const Text = ({ tag = 'b1', children, ...props }: TextProps) => {
   return (
-    <p className={textStyle({ tag: tag })} {...props}>
+    <p className={textStyle({ tag })} {...props}>
       {children}
     </p>
   );
