2741af2336c66b118885ca8adb2c7f0234b7acca
heesunee <heesun729@uos.ac.kr>
2025-01-16 01:53:28 +0900

    feat: Header 컴포넌트 추가 및 배경 레이아웃 설정 (#54)

---
+++ src/pages/mypage/MyPageReservation/index.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@/styles/theme.css';
+
+export const layoutStyle = style({
+  padding: '8.4rem 2rem 0 2rem',
+
+  backgroundColor: vars.colors.gray09,
+});
