05c0369c5a5380bc691c1ef692d35f41f2e46a0f
kimkyuhong <bobo3766@naver.com>
2025-01-16 04:10:20 +0900

    feat: 중간저장 (#63)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,34 @@
+import ClassItem from '@/pages/home/components/ClassItem';
+
 const Home = () => {
-  return <div>홈</div>;
+  return (
+    <ul>
+      <ClassItem
+        lessonId={67890}
+        lessonImageUrl="https://i.namu.wiki/i/B8j2IU2giY7fFeK_M9_QHGinGLoXp---93tHjwIac--A3yKtejXsamDw8sTv-xpjxRQSCfP1fuxRmSW2xFr5NV4_b0-OpTK9_7biMhp4tuuHKExvXq9W-vJHWkSd8SI8hthHj7ctXncHef30HHzC4A.webp"
+        lessonLevel="초급"
+        lessonGenre="힙합"
+        lessonName="기초 힙합 댄스"
+        teacherNickname="김민수"
+        teacherImageUrl="https://i.namu.wiki/i/B8j2IU2giY7fFeK_M9_QHGinGLoXp---93tHjwIac--A3yKtejXsamDw8sTv-xpjxRQSCfP1fuxRmSW2xFr5NV4_b0-OpTK9_7biMhp4tuuHKExvXq9W-vJHWkSd8SI8hthHj7ctXncHef30HHzC4A.webp"
+        lessonStartDateTime="2025-01-15T10:00:00"
+        lessonEndDateTime="2025-01-01T12:00:00"
+        lessonStreetAddress="서울특별시 강남구 테헤란로 123"
+      />
+      <ClassItem
+        lessonId={67890}
+        lessonImageUrl="https://i.namu.wiki/i/B8j2IU2giY7fFeK_M9_QHGinGLoXp---93tHjwIac--A3yKtejXsamDw8sTv-xpjxRQSCfP1fuxRmSW2xFr5NV4_b0-OpTK9_7biMhp4tuuHKExvXq9W-vJHWkSd8SI8hthHj7ctXncHef30HHzC4A.webp"
+        lessonLevel="초급"
+        lessonGenre="힙합"
+        lessonName="기초 힙합 댄스"
+        teacherNickname="김민수"
+        teacherImageUrl="https://i.namu.wiki/i/B8j2IU2giY7fFeK_M9_QHGinGLoXp---93tHjwIac--A3yKtejXsamDw8sTv-xpjxRQSCfP1fuxRmSW2xFr5NV4_b0-OpTK9_7biMhp4tuuHKExvXq9W-vJHWkSd8SI8hthHj7ctXncHef30HHzC4A.webp"
+        lessonStartDateTime="2025-01-10T10:00:00"
+        lessonEndDateTime="2025-01-01T12:00:00"
+        lessonStreetAddress="서울특별시 강남구 테헤란로 123"
+      />
+    </ul>
+  );
 };
 
 export default Home;
