c882e196afed48106334e60204f5d697d0695ccb
constantly-dev <rntqhdl10@naver.com>
2025-01-17 07:12:33 +0900

    feat: title 퍼블리싱 (#79)

---
+++ src/pages/instructorRegister/CareerStep/index.tsx
@@ -0,0 +1,11 @@
+import Description from '../Description';
+
+const CareerStep = () => {
+  return (
+    <>
+      <Description title="학력 및 경력" subTitle="춤에 관련된 것이라면 자유롭게 입력해 보세요" />
+    </>
+  );
+};
+
+export default CareerStep;
