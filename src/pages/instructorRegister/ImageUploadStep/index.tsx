1a878147dae0f7b307a0cfbf32a0885246a35649
constantly-dev <rntqhdl10@naver.com>
2025-01-17 03:04:32 +0900

    feat: 강사 등록 funnel 기본 구조 설정 (#73)

---
+++ src/pages/instructorRegister/ImageUploadStep/index.tsx
@@ -0,0 +1,7 @@
+import ImageUploader from '../ImageUploader';
+
+const ImageUploadStep = () => {
+  return <ImageUploader />;
+};
+
+export default ImageUploadStep;
--- src/pages/instructorRegister/index.tsx
+++ src/pages/instructorRegister/index.tsx
@@ -1,7 +1,36 @@
-import ImageUploader from './ImageUploader';
+import BoxButton from '@/components/BoxButton';
+import Header from '@/components/Header';
+import ProgressBar from '@/components/ProgressBar';
+import { useFunnel } from '@/hooks/useFunnel';
+import ImageUploadStep from './ImageUploadStep';
 
 const InstructorRegister = () => {
-  return <ImageUploader></ImageUploader>;
+  const { Funnel, Step, currentStep, setStep } = useFunnel(6, '/mypage');
+
+  return (
+    <>
+      <Header.Root isColor={true}>
+        <Header.BackIcon />
+        <Header.CloseIcon onClick={() => console.log('hi')} />
+      </Header.Root>
+      <ProgressBar totalStep={5} currentStep={currentStep} />
+
+      <div>
+        <Funnel>
+          <Step name="1">
+            <ImageUploadStep />
+          </Step>
+          <Step name="2">
+            <ImageUploadStep />
+          </Step>
+        </Funnel>
+      </div>
+
+      <div>
+        <BoxButton onClick={() => setStep(1)}>다음으로</BoxButton>
+      </div>
+    </>
+  );
 };
 
 export default InstructorRegister;
