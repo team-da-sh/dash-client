1ca2b0e5e860a606b61458cc3b5f52dab3c01400
heesunee <heesun729@uos.ac.kr>
2025-01-16 17:32:52 +0900

    feat: 초기 구조 세팅 (#88)

---
+++ src/pages/mypage/mypageReservation/index.tsx
@@ -0,0 +1,7 @@
+import * as styles from '@/pages/mypage/mypageReservation/MyPageReservation.css';
+
+const MyPageReservation = () => {
+  return <div>마이페이지 예약 내역</div>;
+};
+
+export default MyPageReservation;
