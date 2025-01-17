26664d841c36244bdd73ae6b1bf6606ca82f73e9
heesunee <heesun729@uos.ac.kr>
2025-01-18 04:25:07 +0900

    feat: Toast 컴포넌트 생성 (#126)

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,7 @@
+import * as styles from '@/components/Toast/index.css';
+
+const Toast = () => {
+  return <div className={styles.toastContainerStyle}>해당 기능은 추후 업데이트 될 예정이에요</div>;
+};
+
+export default Toast;
