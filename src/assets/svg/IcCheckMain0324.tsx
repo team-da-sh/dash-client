7661aa7ac5403b2a9dddf83e29850f39784aaab3
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-17 23:49:38 +0900

    chore: 빌드 오류 잡기 (#93)

--- src/assets/svg/IcCheckMain0324.tsx
+++ src/assets/svg/IcCheckMain0324.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgIcCheckMain0324 = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10" {...props}><path stroke="#8349FF" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.286} d="M1.048 4.429 5.548 9l7.357-8" /></svg>;
 export default SvgIcCheckMain0324;
