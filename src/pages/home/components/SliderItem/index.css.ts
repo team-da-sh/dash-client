36309be8e4ad877015c7f5a9ac3d5b0a7c6bd9dc
kimkyuhong <bobo3766@naver.com>
2025-01-18 01:55:43 +0900

    feat: 광고 한 칸 뷰 구현 (#112)

---
+++ src/pages/home/components/SliderItem/index.css.ts
@@ -0,0 +1,37 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@/styles/theme.css';
+
+export const containerStyle = style({
+  width: '100%',
+  aspectRatio: '1 / 1',
+});
+
+export const imageStyle = style({
+  display: 'block',
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+});
+
+export const descriptionStyle = style({
+  position: 'absolute',
+  left: '2rem',
+  bottom: '5.6rem',
+  maxWidth: '28rem',
+});
+
+export const moreWrapperStyle = style({
+  position: 'absolute',
+  left: '2rem',
+  bottom: '2.8rem',
+});
+
+export const moreButtonStyle = style({
+  color: vars.colors.white,
+  ...vars.fonts.b7,
+
+  border: 'none',
+
+  cursor: 'pointer',
+});
