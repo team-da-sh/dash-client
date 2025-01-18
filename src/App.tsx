9ace9ca34ec0427bc512db8e47d058bfe2a6ca65
heesunee <heesun729@uos.ac.kr>
2025-01-19 00:40:06 +0900

    feat: build 오류 수정 (#126)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
-import { toastContainerStyle } from './components/Toast/index.css';
 import queryClient from './queryClient';
 import './styles/index.css';
 
