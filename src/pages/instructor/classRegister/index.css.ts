7a5401916c300539483a62b58758dfbc6c50471f
constantly-dev <rntqhdl10@naver.com>
2025-01-19 03:02:10 +0900

    feat: 클래스 개설 description 컴포넌트 구현 (#142)

---
+++ src/pages/instructor/classRegister/index.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const containerStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+
+  width: '100%',
+  padding: '8.4rem 2rem 15rem 2rem', //아래 이거 맞을까~!~?
+});
--- src/pages/instructor/classRegister/index.tsx
+++ src/pages/instructor/classRegister/index.tsx
@@ -1,5 +1,19 @@
+import * as styles from '@/pages/instructor/classRegister/index.css';
+import Header from '@/components/Header';
+import Description from './Description';
+
 const ClassRegister = () => {
-  return <div>수업 등록</div>;
+  return (
+    <>
+      <Header.Root>
+        <Header.BackIcon />
+        <Header.Title title="클래스 개설"></Header.Title>
+      </Header.Root>
+      <div className={styles.containerStyle}>
+        <Description title="클래스명" subTitle="돋보일 수 있는 클래스명을 최대 30자 입력해 주세요" />
+      </div>
+    </>
+  );
 };
 
 export default ClassRegister;
