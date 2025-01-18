fd065d2e0c2e00f8d25dd137d6efa6d21f843562
heesunee <heesun729@uos.ac.kr>
2025-01-19 00:07:00 +0900

    feat: notify 생성 (#126)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { toastContainerStyle } from './components/Toast/index.css';
 import queryClient from './queryClient';
 import './styles/index.css';
 
@@ -28,6 +30,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster />
     </QueryClientProvider>
   );
 };
--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,7 +1,15 @@
-import * as styles from '@/components/Toast/index.css';
+import toast from 'react-hot-toast';
+import { vars } from '@/styles/theme.css';
 
-const Toast = () => {
-  return <div className={styles.toastContainerStyle}>해당 기능은 추후 업데이트 될 예정이에요</div>;
-};
+export const notify = () =>
+  toast('해당 기능은 추후 업데이트 될 예정이에요', {
+    duration: 1500,
+    position: 'bottom-center',
+    style: {
+      width: '100%',
 
-export default Toast;
+      backgroundColor: vars.colors.gray10,
+      color: vars.colors.white,
+      ...vars.fonts.b2,
+    },
+  });
