60f7d2fd79f0e7ba2c96f8d2fbf6dd62d7054ae4
kimkyuhong <bobo3766@naver.com>
2025-01-18 16:34:18 +0900

    feat: 중간 저장 (#129)

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,13 @@
+import LoginHeader from '@/pages/login/components/LoginHeader';
+import { IcAsset1 } from '@/assets/svg';
+
 const Login = () => {
-  return <div>로그인</div>;
+  return (
+    <>
+      <LoginHeader />
+      <IcAsset1 width={220} height={220} />
+    </>
+  );
 };
 
 export default Login;
