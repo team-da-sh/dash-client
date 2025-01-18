29efb06028ea718cd6cf033db08f98e344b1828b
constantly-dev <rntqhdl10@naver.com>
2025-01-19 03:41:07 +0900

    feat: 클래스명/클래스 설명 컴포넌트 구현 (#142)

--- src/pages/instructor/classRegister/index.css.ts
+++ src/pages/instructor/classRegister/index.css.ts
@@ -1,3 +1,4 @@
+import { vars } from '@/styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const containerStyle = style({
@@ -7,3 +8,53 @@ export const containerStyle = style({
   width: '100%',
   padding: '8.4rem 2rem 15rem 2rem', //아래 이거 맞을까~!~?
 });
+
+const commonSectionStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '2rem',
+});
+
+// 클래스명명
+export const nameSectionStyle = style([
+  commonSectionStyle,
+  {
+    marginBottom: '2rem',
+  },
+]);
+export const nameLengthStyle = style({
+  alignSelf: 'flex-end',
+});
+
+// 클래스 설명
+export const explanationSectionStyle = style([commonSectionStyle, {}]);
+export const textareaStyle = style({
+  width: '100%',
+  padding: '1.6rem 1.8rem',
+  height: '9.8rem',
+  overflow: 'hidden',
+  resize: 'none',
+  borderRadius: '4px',
+  backgroundColor: vars.colors.gray01,
+  ...vars.fonts.b5,
+
+  '::placeholder': {
+    color: vars.colors.gray05,
+  },
+
+  ':focus': {
+      outline: `1px solid ${vars.colors.main04}`,
+    },
+})
+
+export const imageSectionStyle = style([commonSectionStyle, {}]);
+export const genreSectionStyle = style([commonSectionStyle, {}]);
+
+export const levelSectionStyle = style([commonSectionStyle, {}]);
+export const recommendSectionStyle = style([commonSectionStyle, {}]);
+
+export const scheduleSectionStyle = style([commonSectionStyle, {}]);
+export const personnelSectionStyle = style([commonSectionStyle, {}]);
+
+export const placeSectionStyle = style([commonSectionStyle, {}]);
+export const amountSectionStyle = style([commonSectionStyle, {}]);
--- src/pages/instructor/classRegister/index.tsx
+++ src/pages/instructor/classRegister/index.tsx
@@ -1,8 +1,22 @@
+import { useRef } from 'react';
 import * as styles from '@/pages/instructor/classRegister/index.css';
+import Flex from '@/components/Flex';
 import Header from '@/components/Header';
+import Input from '@/components/Input';
+import Text from '@/components/Text';
 import Description from './Description';
 
 const ClassRegister = () => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleInput = () => {
+    const textArea = textAreaRef.current;
+    if (textArea) {
+      textArea.style.height = '9.8rem'; // Default 높이!
+      textArea.style.height = `${textArea.scrollHeight}px`; // 내용에 따라 높이 조정
+    }
+  };
+
   return (
     <>
       <Header.Root>
@@ -10,7 +24,59 @@ const ClassRegister = () => {
         <Header.Title title="클래스 개설"></Header.Title>
       </Header.Root>
       <div className={styles.containerStyle}>
-        <Description title="클래스명" subTitle="돋보일 수 있는 클래스명을 최대 30자 입력해 주세요" />
+        {/* 클래스명 section */}
+        <section className={styles.nameSectionStyle}>
+          <Description title="클래스명" subTitle="돋보일 수 있는 클래스명을 최대 30자 입력해 주세요" />
+          <Flex direction="column" gap="0.4rem">
+            <Input placeholder="클래스명을 입력해 주세요" />
+            <Text tag="c3" color="gray4" className={styles.nameLengthStyle}>
+              0 / 30
+            </Text>
+          </Flex>
+        </section>
+
+        {/* 클래스 설명 */}
+        <section className={styles.explanationSectionStyle}>
+          <Description title="클래스 설명" subTitle="예비 수강생들을 위해 클래스를 소개해 주세요" />
+          <textarea
+            ref={textAreaRef}
+            onInput={handleInput}
+            placeholder="저는 이런 댄서예요!"
+            className={styles.textareaStyle}></textarea>{' '}
+        </section>
+
+        {/* 클래스 대표 이미지 */}
+        <section className={styles.imageSectionStyle}>
+          <Description title="클래스 대표 이미지" subTitle="대표 이미지는 최대 한 장까지 등록 가능해요" />
+        </section>
+
+        <section className={styles.genreSectionStyle}>
+          <Description title="장르" subTitle="클래스에 해당하는 장르를 최대 2개까지 골라 주세요" />
+        </section>
+
+        <section className={styles.levelSectionStyle}>
+          <Description title="난이도" subTitle="클래스에 해당하는 난이도를 골라주세요" />
+        </section>
+
+        <section className={styles.recommendSectionStyle}>
+          <Description title="클래스 추천 대상" subTitle="어떤 수강생에게 추천하고 싶은지 알려주세요" />
+        </section>
+
+        <section className={styles.scheduleSectionStyle}>
+          <Description title="클래스 일정" subTitle="클래스가 진행될 회차별 날짜와 시간을 등록해 주세요" />
+        </section>
+
+        <section className={styles.personnelSectionStyle}>
+          <Description title="모집 인원" subTitle="원활한 클래스 진행을 위해 최대 인원을 알려주세요" />
+        </section>
+
+        <section className={styles.placeSectionStyle}>
+          <Description title="클래스 장소" subTitle="클래스가 진행될 장소를 알려주세요" />
+        </section>
+
+        <section className={styles.amountSectionStyle}>
+          <Description title="수강료" subTitle="전체 회차를 포함한 최종 금액을 알려주세요" />
+        </section>
       </div>
     </>
   );
