0b9f7ba13a25b21ee5cdf866422c4b8aaa8cdcfd
kimkyuhong <bobo3766@naver.com>
2025-01-18 18:41:02 +0900

    feat: profile 이미지 구현 (#91)

---
+++ src/pages/onboarding/components/ProfileStep/index.css.ts
@@ -0,0 +1,13 @@
+import { style } from '@vanilla-extract/css';
+
+export const profileStyle = style({
+  position: 'relative',
+
+  margin: '39.5 auto 2.4 auto',
+});
+
+export const icCameraStyle = style({
+  position: 'absolute',
+  bottom: '0.6rem',
+  right: '0.6rem',
+});
--- src/pages/onboarding/components/ProfileStep/index.tsx
+++ src/pages/onboarding/components/ProfileStep/index.tsx
@@ -1,7 +1,9 @@
+import { profileStyle } from '@/pages/onboarding/components/ProfileStep/index.css';
 import Flex from '@/components/Flex';
 import Head from '@/components/Head';
 import Input from '@/components/Input';
 import Text from '@/components/Text';
+import { IcCameraMain0624, IcProfileBasic } from '@/assets/svg';
 
 interface ProfileStepProps {}
 
@@ -15,6 +17,11 @@ const ProfileStep = ({}: ProfileStepProps) => {
         <Text tag="b2" color="gray7">
           이유지님의 댄서네임을 알려주세요
         </Text>
+      </Flex>
+
+      <Flex className={profileStyle}>
+        <IcProfileBasic width={100} height={100} />
+        <IcCameraMain0624 width={24} height={24} />
       </Flex>
 
       <Flex direction="column" gap="1.6rem" marginTop="2.8rem" width="100%">
