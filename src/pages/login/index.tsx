374c78435785897a79f6d8d020b36b8f67ef0e67
kimkyuhong <bobo3766@naver.com>
2025-01-19 14:35:31 +0900

    feat: 카카오 로그인 뷰 구현 (#129)

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import LoginHeader from '@/pages/login/components/LoginHeader';
 import { containerStyle } from '@/pages/login/index.css';
 import Flex from '@/components/Flex';
 import Head from '@/components/Head';
-import { IcAsset1 } from '@/assets/svg';
+import loginGif from '@/../public/gif/login.gif';
 
 const Login = () => {
   return (
@@ -11,8 +11,11 @@ const Login = () => {
       <LoginHeader />
 
       <Flex direction="column" gap="3.2rem" className={containerStyle}>
-        <IcAsset1 width={220} height={220} />
-        <Flex direction="column">
+        <Flex width="100%" justify="center">
+          <img src={loginGif} width={300} />
+        </Flex>
+
+        <Flex direction="column" width="100%">
           <Head tag="h2">댄스 클래스로의 첫 걸음,</Head>
           <Head tag="h2">지금 시작해 보세요</Head>
         </Flex>
