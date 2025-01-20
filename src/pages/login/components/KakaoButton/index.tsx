7d66e0181ae9f5337eda52bb477c8c8c94bc7b6f
kimkyuhong <bobo3766@naver.com>
2025-01-21 06:05:08 +0900

    fix: api 키 설정 변경 (#129)

--- src/pages/login/components/KakaoButton/index.tsx
+++ src/pages/login/components/KakaoButton/index.tsx
@@ -3,14 +3,11 @@ import Flex from '@/components/Flex';
 import Head from '@/components/Head';
 import { IcKakaoKakaobrown28 } from '@/assets/svg';
 
-interface KakaoButtonProps {}
-
-const KakaoButton = ({}: KakaoButtonProps) => {
-  // 테스트용 키, 실제 배포 사이트용은 env로 분리할 예정
-  const Rest_api_key = '802706f4d7371ae8587dec525ed29979'; //REST API KEY
+const KakaoButton = () => {
   const redirect_uri = 'http://localhost:5173/auth'; //Redirect URI
-  // oauth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+
+  // auth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.REST_API_KEY}&redirect_uri=${redirect_uri}&response_type=code`;
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
