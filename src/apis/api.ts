8f22c425bf8317d5a7e47f0567cc60c5c9e6751c
kimkyuhong <bobo3766@naver.com>
2025-01-21 02:41:44 +0900

    feat: 중간 저장 (#162)

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,42 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-export const instance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_DEV_BASE_URL,
 });
 
-// 필요 시 interceptor 추가
+const navigate = useNavigate();
+
+// 요청 전 header에 accesstoken 입력
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const accessToken = localStorage.getItem('ACCESS_TOKEN');
+
+    if (accessToken) {
+      const newConfig = { ...config };
+      newConfig.headers.Authorization = `Bearer ${accessToken}`;
+      return newConfig;
+    }
+
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const statusCode = error.response?.status;
+    if (statusCode === 401) {
+      error.response.statuesText = 'Unauthorized';
+      error.response.status = 401;
+      navigate('/login');
+    }
+    return Promise.reject(error);
+  }
+);
