2361485d5e5d768ea5fa077fff6825c31d9d1f56
heesunee <heesun729@uos.ac.kr>
2025-01-21 18:10:54 +0900

    feat: 시간 선택 퍼블리싱 (#172)

--- src/pages/instructor/classRegister/ClassSchedule/TimeStep/index.tsx
+++ src/pages/instructor/classRegister/ClassSchedule/TimeStep/index.tsx
@@ -1,5 +1,44 @@
+import { useState } from 'react';
+import Flex from '@/components/Flex';
+import Head from '@/components/Head';
+import { increaseHour, decreaseHour, increaseMinute, decreaseMinute, toggleAmpm } from '@/utils/timeUtils';
+import TimeControl from './TimeSelector';
+
 const TimeStep = () => {
-  return <div>시간 선택~!!</div>;
+  const [hour, setHour] = useState(12); // 초기값 12시
+  const [minute, setMinute] = useState(0); // 00분
+  const [ampm, setAmpm] = useState('AM'); // AM/PM
+
+  return (
+    <Flex direction="column" paddingLeft="2rem" paddingRight="2rem">
+      <Head level="h2" tag="h6">
+        클래스 시작 시간
+      </Head>
+      <Flex justify="center" gap="1rem" paddingTop="1.6rem" paddingBottom="2.4rem">
+        <TimeControl
+          label="hour"
+          value={hour}
+          onIncrease={() => setHour(increaseHour(hour))}
+          onDecrease={() => setHour(decreaseHour(hour))}
+        />
+        <TimeControl
+          label="minute"
+          value={minute}
+          onIncrease={() => setMinute(increaseMinute({ currentMinute: minute, setHour }))}
+          onDecrease={() => setMinute(decreaseMinute({ currentMinute: minute, setHour }))}
+        />
+        <TimeControl
+          label="ampm"
+          value={ampm}
+          onIncrease={() => setAmpm(toggleAmpm(ampm))}
+          onDecrease={() => setAmpm(toggleAmpm(ampm))}
+        />
+      </Flex>
+      <Head level="h2" tag="h6">
+        클래스 진행 시간
+      </Head>
+    </Flex>
+  );
 };
 
 export default TimeStep;
