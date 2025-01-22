88c3ef4de518251ffa8086c03953ce1ee52b9edb
constantly-dev <rntqhdl10@naver.com>
2025-01-23 06:25:08 +0900

    feat: 클래스 등록 axios 함수 구현 (#206)

--- src/apis/instructor/classRegister/axios/index.ts
+++ src/apis/instructor/classRegister/axios/index.ts
@@ -10,3 +10,31 @@ export const getLocationList = async (query: string) => {
 
   return data;
 };
+
+interface ClassTimeTypes {
+  startTime: string;
+  endTime: string;
+}
+
+interface ClassRegisterInfoTypes {
+  imageUrls: string[];
+  name: string;
+  detail: string;
+  videoUrl: string[];
+  maxReservationCount: number;
+  genre: string;
+  level: string;
+  recommendation: string;
+  price: number;
+  location: string;
+  streetAddress: string;
+  oldStreetAddress: string;
+  detailedAddress: string;
+  times: ClassTimeTypes[];
+}
+
+export const postClassRegisterInfo = async (infoData: ClassRegisterInfoTypes) => {
+  const { data } = await instance.post(API_URL.LESSONS, infoData);
+
+  return data;
+};
