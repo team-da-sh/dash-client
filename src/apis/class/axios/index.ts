ebb0b9a7ec26585baadd478988fa12ba0ba0d370
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-23 03:47:26 +0900

    api: contants에 /{lessonId} 제거 (#169)

--- src/apis/class/axios/index.ts
+++ src/apis/class/axios/index.ts
@@ -3,7 +3,7 @@ import { API_URL } from "@/apis/constants/apiURL";
 import { LessonDetail } from '@/pages/class/types';
 
 export const getLessonDetail = async (lessonId: string): Promise<LessonDetail> => {
-  const url = API_URL.LESSON_DETAIL.replace('{lessonId}', lessonId);
+  const url = `${API_URL.LESSON_DETAIL}/${lessonId}`;
 
   const { data } = await instance.get(url);
   return data;
