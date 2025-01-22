32f63dfe4fa943ddef952f04c09d33778c728a6d
constantly-dev <rntqhdl10@naver.com>
2025-01-23 06:28:29 +0900

    feat: 클래스 등록 query 함수 구현 (#206)

--- src/apis/instructor/classRegister/queries/index.ts
+++ src/apis/instructor/classRegister/queries/index.ts
@@ -1,6 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from '@/apis/constants/queryKey';
-import { getLocationList } from '../axios';
+import { getLocationList, postClassRegisterInfo } from '../axios';
 
 // TODO: keyword 필수인지 질문
 export const useGetLocationList = (query: string) => {
@@ -9,5 +9,33 @@ export const useGetLocationList = (query: string) => {
     queryFn: async () => {
       return await getLocationList(query);
     },
+  });
+};
+
+interface ClassTimeTypes {
+  startTime: string;
+  endTime: string;
+}
+
+interface ClassRegisterInfoTypes {
+  imageUrls: string[];
+  name: string;
+  detail: string;
+  videoUrl: string[];
+  maxReservationCount: number;
+  genre: string;
+  level: string;
+  recommendation: string;
+  price: number;
+  location: string;
+  streetAddress: string;
+  oldStreetAddress: string;
+  detailedAddress: string;
+  times: ClassTimeTypes[];
+}
+
+export const usePostClassRegisterInfo = () => {
+  return useMutation({
+    mutationFn: (infoData: ClassRegisterInfoTypes) => postClassRegisterInfo(infoData),
   });
 };
