05cba1d96b5bfbb4975e216b08ba34b47af05966
constantly-dev <rntqhdl10@naver.com>
2025-01-23 18:21:39 +0900

    feat: 강사 등록 페이지 퍼널에서 close button navigate 추가 (#206)

--- src/pages/instructorRegister/index.tsx
+++ src/pages/instructorRegister/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import InstructorRegisterFunnel from '@/pages/instructorRegister/components/InstructorRegisterFunnel';
 import { progressBarCustomStyle } from '@/pages/instructorRegister/index.css';
 import Header from '@/components/Header';
@@ -8,12 +9,13 @@ import { ROUTES_CONFIG } from '@/routes/routesConfig';
 const InstructorRegister = () => {
   const TOTAL_STEP = 6;
   const { Funnel, Step, currentStep, setStep } = useFunnel(TOTAL_STEP, ROUTES_CONFIG.home.path);
+  const navigate = useNavigate();
 
   return (
     <>
       <Header.Root isColor={true}>
         {currentStep < TOTAL_STEP && <Header.BackIcon onFunnelBackClick={() => setStep(-1)} />}
-        <Header.CloseIcon onClick={() => console.log('hi')} />
+        <Header.CloseIcon onClick={() => navigate(ROUTES_CONFIG.home.path)} />
       </Header.Root>
       {currentStep < TOTAL_STEP && (
         <ProgressBar totalStep={5} currentStep={currentStep} className={progressBarCustomStyle} />
