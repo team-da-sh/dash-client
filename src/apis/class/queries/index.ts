fb8bf4651713d1ba87e5cdba8a18224afe3d46b3
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-01-23 11:37:50 +0900

    api: 쿼리키 변경 (#184)

--- src/apis/class/queries/index.ts
+++ src/apis/class/queries/index.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { LessonDetailApiResponse } from '@/pages/class/types';
 import { getLessonDetail } from '@/apis/class/axios';
-import { LessonDetailApiResponse } from "@/pages/class/types";
-import { QUERY_KEYS } from "@/apis/constants/queryKey";
+import { QUERY_KEYS } from '@/apis/constants/queryKey';
 
 export const useGetLessonDetail = (lessonId: string) => {
   return useQuery<LessonDetailApiResponse, Error>({
-    queryKey: [QUERY_KEYS.LESSON_DETAIL],
+    queryKey: [QUERY_KEYS.LESSON_DETAIL, lessonId],
     queryFn: () => getLessonDetail(lessonId),
   });
 };
