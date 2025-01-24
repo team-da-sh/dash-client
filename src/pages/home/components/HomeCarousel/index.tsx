6e0f26334784d364ab95fc5575c0b1878e57f5a8
kimkyuhong <bobo3766@naver.com>
2025-01-24 19:01:33 +0900

    fix: home 배너 넣기 (#271)

--- src/pages/home/components/HomeCarousel/index.tsx
+++ src/pages/home/components/HomeCarousel/index.tsx
@@ -8,10 +8,15 @@ import { BannerImg, ChoreohongImg, KkukgirlImg } from '@/assets/images';
 const ADVERTISEMENTS = [
   {
     imageUrl: KkukgirlImg,
-    description: '5주 집중 커리큘럼, 스걸파 출연 댄서의 피메일 힙합 기본기 튼튼하게 다지기',
+    description: `5주 집중 커리큘럼, 스걸파 출연 댄서의 `,
+    description2: '피메일 힙합 기본기 튼튼하게 다지기',
   },
-  { imageUrl: ChoreohongImg, description: '코레홍과 함께하는 NMIXX-DASH 코레오그래피' },
-  { imageUrl: BannerImg, description: '' },
+  { imageUrl: ChoreohongImg, description: '코레홍과 함께하는', description2: 'NMIXX-DASH 코레오그래피' },
+  {
+    imageUrl: BannerImg,
+    description: '알고 싶던 댄서나 관심있는 클래스를 ',
+    description2: '한 눈에 모아볼 수 있어요!',
+  },
 ];
 const HomeCarousel = () => {
   return (
@@ -28,7 +33,7 @@ const HomeCarousel = () => {
       {ADVERTISEMENTS.map((data, index) => {
         return (
           <SwiperSlide key={`${index}-${data.description}`}>
-            <SliderItem imageUrl={data.imageUrl} description={data.description} />
+            <SliderItem imageUrl={data.imageUrl} description={data.description} description2={data.description2} />
           </SwiperSlide>
         );
       })}
