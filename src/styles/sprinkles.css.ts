84d193e8cd7d06969b30a4991bc615c1aa8314a9
kimkyuhong <bobo3766@naver.com>
2025-02-17 14:26:25 +0900

    feat: flex 관련 속성 sprinkles 설정

---
+++ src/styles/sprinkles.css.ts
@@ -0,0 +1,63 @@
+import { defineProperties, createSprinkles } from '@vanilla-extract/sprinkles';
+
+const createNumberArray = (length: number) => {
+  return Array.from({ length: length }, (_, i) => i + 1);
+};
+
+const createEvenNumberArray = (length: number) => {
+  return Array.from({ length: length }, (_, i) => 2 * (i + 1));
+};
+
+const createPercentArray = () => {
+  return Array.from({ length: 100 }, (_, i) => `${i + 1}%`);
+};
+
+const MIN_WIDTH = 375;
+
+const flexProperties = defineProperties({
+  properties: {
+    display: ['flex'],
+    flexDirection: ['row', 'column'],
+    justifyContent: ['stretch', 'flex-start', 'center', 'flex-end', 'space-around', 'space-between'],
+    alignItems: ['stretch', 'flex-start', 'center', 'flex-end'],
+    flexGrow: [0, 1],
+    flexShrink: [0, 1],
+    flexWrap: ['nowrap', 'wrap', 'wrap-reverse'],
+    gap: createEvenNumberArray(50),
+  },
+});
+
+const layoutProperties = defineProperties({
+  properties: {
+    position: ['absolute', 'fixed', 'relative', 'static', 'sticky'],
+    width: [...createNumberArray(MIN_WIDTH), ...createPercentArray()],
+    height: [...createNumberArray(MIN_WIDTH), ...createPercentArray()],
+    marginTop: createNumberArray(MIN_WIDTH),
+    marginBottom: createNumberArray(MIN_WIDTH),
+    marginRight: createNumberArray(MIN_WIDTH),
+    marginLeft: createNumberArray(MIN_WIDTH),
+    paddingTop: createNumberArray(MIN_WIDTH),
+    paddingBottom: createNumberArray(MIN_WIDTH),
+    paddingRight: createNumberArray(MIN_WIDTH),
+    paddingLeft: createNumberArray(MIN_WIDTH),
+  },
+  shorthands: {
+    p: ['paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft'],
+    px: ['paddingLeft', 'paddingRight'],
+    py: ['paddingTop', 'paddingBottom'],
+    pt: ['paddingTop'],
+    pb: ['paddingBottom'],
+    pl: ['paddingLeft'],
+    pr: ['paddingRight'],
+
+    m: ['marginTop', 'marginRight', 'marginBottom', 'marginLeft'],
+    mx: ['marginLeft', 'marginRight'],
+    my: ['marginTop', 'marginBottom'],
+    mt: ['marginTop'],
+    mb: ['marginBottom'],
+    ml: ['marginLeft'],
+    mr: ['marginRight'],
+  },
+});
+
+export const sprinkles = createSprinkles(flexProperties, layoutProperties);
