4b5cdba4f0a984f1214d1dbe1c31a48c3e673efe
kimkyuhong <bobo3766@naver.com>
2025-03-20 00:19:44 +0900

    chore: 주석 제거 (#325)

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,6 @@
 import KakaoButton from '@/pages/login/components/KakaoButton/KakaoButton';
 import LoginHeader from '@/pages/login/components/LoginHeader/LoginHeader';
 import { containerStyle } from '@/pages/login/login.css';
-// import { LoginGif } from '@/shared/assets/gif';
 import LoginWepm from '@/shared/assets/webm/login.webm';
 import Flex from '@/shared/components/Flex/Flex';
 import Head from '@/shared/components/Head/Head';
@@ -16,7 +15,6 @@ const Login = () => {
           <video width={300} autoPlay muted loop>
             <source src={LoginWepm} type="video/webm" />
           </video>
-          {/* <img src={LoginGif} width={300} /> */}
         </Flex>
 
         <Flex direction="column" width="100%">
