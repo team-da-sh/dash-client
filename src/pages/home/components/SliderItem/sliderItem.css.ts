20ec9682967f2d30787edf946745aa0ed2680b9f
kimkyuhong <bobo3766@naver.com>
2025-03-26 17:50:04 +0900

    refactor: 홈 캐러셀 코드 리펙토링 (#338)

--- src/pages/home/components/SliderItem/sliderItem.css.ts
+++ src/pages/home/components/SliderItem/sliderItem.css.ts
@@ -2,6 +2,9 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '@/shared/styles/theme.css';
 
 export const containerStyle = style({
+  display: 'flex',
+  position: 'relative',
+
   width: '100%',
   aspectRatio: '1 / 1',
 });
@@ -14,23 +17,21 @@ export const imageStyle = style({
   objectFit: 'cover',
 });
 
-export const description2Style = style({
-  position: 'absolute',
-  left: '2rem',
-  bottom: '5.6rem',
-  maxWidth: '34rem',
-  wordBreak: 'keep-all',
-});
-
 export const descriptionStyle = style({
   position: 'absolute',
   left: '2rem',
   bottom: '9rem',
   maxWidth: '34rem',
+
   wordBreak: 'keep-all',
+  whiteSpace: 'pre-line',
 });
 
 export const moreWrapperStyle = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.2rem',
+
   position: 'absolute',
   left: '2rem',
   bottom: '2.8rem',
