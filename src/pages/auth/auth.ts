d3621c16ba056b0ec2a39c9c2f145d441a764716
kimkyuhong <bobo3766@naver.com>
2025-04-03 23:21:28 +0900

    chore: 자잘한 수정

--- src/pages/auth/auth.ts
+++ src/pages/auth/auth.ts
@@ -8,12 +8,11 @@ export const LoginCallback = () => {
   // 인가 코드 추출
   const code = params.get('code');
 
-  // 해당 코드로 소셜 로그인 api 쏘기
   const { mutate: login } = useLoginMutation();
 
   useEffect(() => {
     if (code) login({ redirectUrl: import.meta.env.VITE_KAKAO_REDIRECT_URI, code });
-  }, [code]);
+  }, [code, login]);
 
   // 잠시 인가 코드만 추출해서 api를 보내는 페이지이기 때문에 null 값 반환
   return null;
