3001203c32e94e91f475bcabddb60d097c30a0cf
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-04-13 01:31:50 +0900

    fix: 빌드 에러 해결 (#363)

--- src/pages/search/apis/queries.ts
+++ src/pages/search/apis/queries.ts
@@ -1,10 +1,10 @@
 import type { UseQueryResult } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
 import { getClassList, getDancerList } from '@/pages/search/apis/axios';
-import type { ClassListParams, ClassListResponse, DancerListResponse } from '@/pages/search/types/api';
+import type { ClassListParamsTypes, ClassListResponseTypes, DancerListResponseTypes } from '@/pages/search/types/api';
 
-export const useGetDancerList = (params: { keyword?: string }): UseQueryResult<DancerListResponse, Error> => {
-  return useQuery<DancerListResponse, Error>({
+export const useGetDancerList = (params: { keyword?: string }): UseQueryResult<DancerListResponseTypes, Error> => {
+  return useQuery<DancerListResponseTypes, Error>({
     queryKey: ['dancerList', params],
     queryFn: async () => {
       return await getDancerList(params);
@@ -13,8 +13,8 @@ export const useGetDancerList = (params: { keyword?: string }): UseQueryResult<D
   });
 };
 
-export const useGetClassList = (params: ClassListParams): UseQueryResult<ClassListResponse, Error> => {
-  return useQuery<ClassListResponse, Error>({
+export const useGetClassList = (params: ClassListParamsTypes): UseQueryResult<ClassListResponseTypes, Error> => {
+  return useQuery<ClassListResponseTypes, Error>({
     queryKey: ['classList', params],
     queryFn: async () => {
       return await getClassList(params);
