96a5000a8e70d0841a4cd04cef3649b8ed563c9d
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-04-16 00:17:48 +0900

    feat: 중간 작업 (#362)

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,20 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
+import { ROUTES_CONFIG } from '@/routes/routesConfig';
 import Header from '@/shared/components/Header/Header';
 
 const Layout = () => {
+  const location = useLocation();
+
+  const hideHeaderPaths = [ROUTES_CONFIG.search.path, ROUTES_CONFIG.onboarding.path];
+
+  // 현재 경로가 hideHeaderPaths 중 하나로 시작하는지 확인
+  const shouldHideHeader = hideHeaderPaths.some(
+    (path) => location.pathname === path || location.pathname.startsWith(`${path}`)
+  );
+
   return (
     <>
-      <Header />
+      {!shouldHideHeader && <Header />}
       <Outlet />
       <ScrollRestoration />
     </>
--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,13 +1,18 @@
+import { useState, Suspense } from 'react';
 import { useNavigate } from 'react-router-dom';
+import MyPage from '@/pages/home/components/MyPage/MyPage';
+import { overlayActiveStyle, overlayStyle } from '@/pages/home/home.css';
 import { ROUTES_CONFIG } from '@/routes/routesConfig';
 import IcLogoSmallBlack from '@/shared/assets/svg/IcLogoSmallBlack';
 import IcMypageBlack24 from '@/shared/assets/svg/IcMypageBlack24';
 import IcSearchBlack24 from '@/shared/assets/svg/IcSearchBlack24';
 import { sprinkles } from '@/shared/styles/sprinkles.css';
+import { isLoggedIn } from '@/shared/utils/authUtil';
 import { headerStyle } from './header.css';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [showMyPage, setShowMyPage] = useState(false);
 
   const handleLogoClick = () => {
     navigate(ROUTES_CONFIG.home.path);
@@ -18,11 +23,26 @@ const Header = () => {
   };
 
   const handleMypageClick = () => {
+    if (!isLoggedIn()) {
+      navigate(ROUTES_CONFIG.login.path);
+      return;
+    }
+    setShowMyPage(!showMyPage);
     // navigate(ROUTES_CONFIG.mypage.path); -> mypage 페이지화 작업되면 적용 예정
+  };
+
+  const handleCloseMyPageClick = () => {
+    setShowMyPage(false);
   };
 
   return (
     <header className={headerStyle}>
+      <div className={`${overlayStyle} ${showMyPage ? overlayActiveStyle : ''}`} />
+      {showMyPage && (
+        <Suspense fallback={<div />}>
+          <MyPage showMyPage={showMyPage} onClose={handleCloseMyPageClick} />
+        </Suspense>
+      )}
       <IcLogoSmallBlack width={58} height={20} onClick={handleLogoClick} />
       <div className={sprinkles({ display: 'flex', gap: 20 })}>
         <IcSearchBlack24 width={24} height={24} onClick={handleSearchClick} />
