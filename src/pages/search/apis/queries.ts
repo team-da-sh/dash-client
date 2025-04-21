b13be88cd534f780ca57c797e418b42760946656
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-04-21 14:43:09 +0900

    code review: AxiosError 타입 사용하도록 수정 (#363)

--- src/pages/search/apis/queries.ts
+++ src/pages/search/apis/queries.ts
@@ -1,10 +1,11 @@
 import type { UseQueryResult } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
 import { getClassList, getDancerList } from '@/pages/search/apis/axios';
 import type { ClassListParamsTypes, ClassListResponseTypes, DancerListResponseTypes } from '@/pages/search/types/api';
 
-export const useGetDancerList = (params: { keyword?: string }): UseQueryResult<DancerListResponseTypes, Error> => {
-  return useQuery<DancerListResponseTypes, Error>({
+export const useGetDancerList = (params: { keyword?: string }): UseQueryResult<DancerListResponseTypes, AxiosError> => {
+  return useQuery<DancerListResponseTypes, AxiosError>({
     queryKey: ['dancerList', params],
     queryFn: async () => {
       return await getDancerList(params);
@@ -13,8 +14,8 @@ export const useGetDancerList = (params: { keyword?: string }): UseQueryResult<D
   });
 };
 
-export const useGetClassList = (params: ClassListParamsTypes): UseQueryResult<ClassListResponseTypes, Error> => {
-  return useQuery<ClassListResponseTypes, Error>({
+export const useGetClassList = (params: ClassListParamsTypes): UseQueryResult<ClassListResponseTypes, AxiosError> => {
+  return useQuery<ClassListResponseTypes, AxiosError>({
     queryKey: ['classList', params],
     queryFn: async () => {
       return await getClassList(params);
