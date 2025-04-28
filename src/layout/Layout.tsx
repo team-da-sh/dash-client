8528767fb309b38da76170bff71fc5c431588963
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-04-28 15:39:29 +0900

    code review: 희선 코리 반영 (#362)

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,10 +3,10 @@ import { ROUTES_CONFIG } from '@/routes/routesConfig';
 import Header from '@/shared/components/Header/Header';
 
 const Layout = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
-  const isSearchPath = location.pathname === ROUTES_CONFIG.search.path;
-  const isOnboardingPath = location.pathname === ROUTES_CONFIG.onboarding.path;
+  const isSearchPath = pathname === ROUTES_CONFIG.search.path;
+  const isOnboardingPath = pathname === ROUTES_CONFIG.onboarding.path;
 
   const shouldHideHeader = isSearchPath || isOnboardingPath;
 
