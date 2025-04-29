8ea0147b14cfb49b3f9efa96ed785880025d2e5f
heesunee <heesun729@uos.ac.kr>
2025-04-30 02:28:09 +0900

    feat: 수강생 nav 버튼 구현 (#393)

--- src/pages/mypage/components/StudentContent/StudentContent.tsx
+++ src/pages/mypage/components/StudentContent/StudentContent.tsx
@@ -1,5 +1,8 @@
 import ClassProgress from '@/pages/mypage/components/ClassProgress/ClassProgress';
+import MenuButton from '@/pages/mypage/components/MenuButton/MenuButton';
 import * as styles from '@/pages/mypage/components/StudentContent/studentContent.css';
+import { MENU_LIST } from '@/pages/mypage/constants/myPageList';
+import Divider from '@/shared/components/Divider/Divider';
 import InfoComponent from '@/shared/components/InfoComponent/InfoComponent';
 import Text from '@/shared/components/Text/Text';
 import { mockMyPageData } from '../TabWrapper/mockData';
@@ -8,16 +11,24 @@ const StudentContent = () => {
   const data = mockMyPageData;
   return (
     <div className={styles.containerStyle}>
-      <InfoComponent
-        profileImageUrl={data.profileImageUrl}
-        mainText={<Text tag="b1_sb">{data.nickname}</Text>}
-        subContent={
-          <Text tag="b3_m" color="gray6">
-            {data.name} · {data.phoneNumber}
-          </Text>
-        }
-      />
-      <ClassProgress />
+      <div className={styles.topContainerStyle}>
+        <InfoComponent
+          profileImageUrl={data.profileImageUrl}
+          mainText={<Text tag="b1_sb">{data.nickname}</Text>}
+          subContent={
+            <Text tag="b3_m" color="gray6">
+              {data.name} · {data.phoneNumber}
+            </Text>
+          }
+        />
+        <ClassProgress />
+      </div>
+      <div className={styles.menuButtonContainerStyle}>
+        {MENU_LIST.map((item) => (
+          <MenuButton key={item.path} {...item} />
+        ))}
+      </div>
+      <Divider color="gray1" thickness="0.4rem" />
     </div>
   );
 };
--- src/pages/mypage/constants/myPageList.ts
+++ src/pages/mypage/constants/myPageList.ts
@@ -1,4 +1,5 @@
 import { ROUTES_CONFIG } from '@/routes/routesConfig';
+import SvgIcKakaoKakaobrown28 from '@/shared/assets/svg/IcKakaoKakaobrown28';
 
 export const LIST_DATA = [
   { label: '내 정보 수정', id: 1, inActive: true },
@@ -14,4 +15,27 @@ export const LIST_DATA = [
   { label: '자주 묻는 질문', id: 5, inActive: true },
   { label: '고객 센터', id: 6, hasDivider: true, inActive: true },
   { label: '로그아웃', id: 7 },
+];
+
+export const MENU_LIST = [
+  {
+    icon: SvgIcKakaoKakaobrown28,
+    label: '수강 목록',
+    path: '/courses',
+  },
+  {
+    icon: SvgIcKakaoKakaobrown28,
+    label: '관심 목록',
+    path: '/favorites',
+  },
+  {
+    icon: SvgIcKakaoKakaobrown28,
+    label: '최근 본',
+    path: '/recent',
+  },
+  {
+    icon: SvgIcKakaoKakaobrown28,
+    label: '리뷰 작성',
+    path: '/review',
+  },
 ];
