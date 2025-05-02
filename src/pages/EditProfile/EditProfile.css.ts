f85cce5d3f35747f46aa1e075a5a3c707e5b9c43
heesunee <heesun729@uos.ac.kr>
2025-05-03 04:23:10 +0900

    feat: 이름 수정만 완성 (#393)

---
+++ src/pages/EditProfile/EditProfile.css.ts
@@ -0,0 +1,13 @@
+import { style } from '@vanilla-extract/css';
+
+export const layoutStyle = style({
+  padding: '2rem',
+});
+
+export const inputWrapperStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+
+  padding: '1.2rem 0 1.2rem 0',
+});
--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -1,37 +1,63 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import { useForm } from 'react-hook-form';
 import Head from '@/shared/components/Head/Head';
 import Input from '@/shared/components/Input/Input';
+import Text from '@/shared/components/Text/Text';
+import { mockMyPageData } from '../mypage/components/TabWrapper/mockData';
+import * as styles from './EditProfile.css';
 
-const userSchema = z.object({
-  profilePhoto: z
-    .any() // FileList 타입이므로 any로 잡은 뒤 refine
-    .refine((files) => {
-      if (!(files instanceof FileList)) return false;
-      return files.length <= 1;
-    }, '프로필 사진은 최대 1장까지 업로드 가능합니다'),
-
+const profileSchema = z.object({
   nickname: z
     .string()
-    .min(1, '댄서 네임을 입력해주세요')
-    .max(20, '20자 이내로 입력해주세요')
-    .regex(/^[\w가-힣]+$/, '특수기호 및 공백 없이 입력해주세요'),
-
-  phoneNumber: z
-    .string()
-    .length(11, '숫자 11자리를 입력해주세요')
-    .regex(/^[0-9]\d{10}$/, '숫자만 11자리 입력해주세요'),
+    .min(1, '댄서네임을 입력해주세요')
+    .regex(/^[\w가-힣]+$/, '특수기호, 띄어쓰기는 입력할 수 없어요'),
 });
 
+type profileFormValues = z.infer<typeof profileSchema>;
+
 const EditProfile = () => {
+  const data = mockMyPageData;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<profileFormValues>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      nickname: data.nickname,
+    },
+  });
+
+  // 폼 제출 처리, 임시 로직
+  const onSubmit = (formData: profileFormValues) => {
+    console.log('제출성공', formData);
+
+    alert('성공적으로 변경되었습니다.');
+  };
+
   return (
-    <main>
+    <div className={styles.layoutStyle}>
       <Head level="h2" tag="h6_sb">
         내 정보 수정
       </Head>
-      <section>
-        <Input placeholder="댄서네임" />
-      </section>
-    </main>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.inputWrapperStyle}>
+          <label>
+            <Text tag="b2_sb">댄서네임</Text>
+          </label>
+          <Input placeholder="댄서네임을 입력해주세요" {...register('nickname')} isError={!!errors.nickname} />
+          {errors.nickname && (
+            <Text tag="b3_r" color="alert3">
+              {errors.nickname.message}
+            </Text>
+          )}
+        </div>
+        <button type="submit">확인</button>
+      </form>
+    </div>
   );
 };
 
