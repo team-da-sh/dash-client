1ec09e735ce4fff4653489a06ba16ea2e01b415e
heesunee <heesun729@uos.ac.kr>
2025-05-08 16:42:57 +0900

    fix: 수정이 코드 누락 추가 (#392)

--- src/pages/dancer/utils/url.ts
+++ src/pages/dancer/utils/url.ts
@@ -1,3 +1,10 @@
+export const extractUsernameFromUrl = (url?: string): string => {
+  if (!url) return '';
+
+  const cleanedUrl = url.split('?')[0];
+  return cleanedUrl.split('/').filter(Boolean).pop() || '';
+};
+
 const isValidUrl = (url?: string) => {
   try {
     return url ? new URL(url) : null;
