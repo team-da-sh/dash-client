9ab25b91049cf9e4511997c25e2cda4fad3eb23d
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2025-05-18 21:20:45 +0900

    refactor: error 페이지 qa 반영 (#430)

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -23,18 +23,18 @@ const Error = () => {
       paddingLeft="2rem"
       paddingRight="2rem"
       paddingBottom="2.4rem"
-      paddingTop="11.2rem"
+      paddingTop="6rem"
       align="center"
       justify="center">
       <Flex direction="column" gap="1.4rem" align="center" marginBottom="2.9rem">
         <Head tag="h5_sb">서비스 이용에 불편을 드려 죄송합니다.</Head>
-        <Flex direction="column" align="center">
-          <Text tag="b3_m">요청하신 페이지를 처리하는 도중 예기치 못한 에러가 발생했습니다.</Text>
+        <Flex direction="column" align="center" gap="0.2rem">
+          <Text tag="b3_m">요청하신 페이지를 처리하는 도중 예기치 못한 에러가 발생했어요.</Text>
           <Text tag="b3_m">다시 한 번 시도하거나 홈으로 이동해 주세요.</Text>
         </Flex>
       </Flex>
       <Suspense fallback={<div>Loading...</div>}>
-        <IcError />
+        <IcError width={300} height={300} />
       </Suspense>
       <BoxButton className={boxButtonStyle} onClick={handleHomeNavigation}>
         홈으로 이동
