b3f9bb6a57af99c01fe4d736eed2f85660d81c20
heesunee <heesun729@uos.ac.kr>
2025-05-18 20:17:51 +0900

    feat: 토스트 메세지 수정 (#433)

--- src/pages/editProfiles/api/queries.ts
+++ src/pages/editProfiles/api/queries.ts
@@ -4,15 +4,19 @@ import { patchMyProfile } from '@/pages/editProfiles/api/axios';
 import { UpdateProfileRequestTypes } from '@/pages/editProfiles/types/api';
 import { ROUTES_CONFIG } from '@/routes/routesConfig';
 import queryClient from '@/queryClient';
+import { notify } from '@/shared/components/Toast/Toast';
 import { QUERY_KEYS } from '@/shared/constants/queryKey';
 
 export const usePatchMyProfile = () => {
   const navigate = useNavigate();
+
   return useMutation({
     mutationFn: (profileData: UpdateProfileRequestTypes) => patchMyProfile(profileData),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.MEMBERS_ME] });
       navigate(ROUTES_CONFIG.mypage.path);
+
+      notify('프로필 수정이 완료되었어요!', true);
     },
     onError: (error: Error) => {
       navigate(ROUTES_CONFIG.error.path);
--- src/shared/components/Toast/Toast.tsx
+++ src/shared/components/Toast/Toast.tsx
@@ -1,20 +1,32 @@
 import toast from 'react-hot-toast';
+import IcClearMain from '@/shared/assets/svg/IcClearMain';
 import { vars } from '@/shared/styles/theme.css';
 
-export const notify = (text: string) => {
+export const notify = (message: string, icon?: boolean) => {
   toast.dismiss();
 
-  toast(text, {
-    duration: 1500,
-    position: 'bottom-center',
-    style: {
-      width: '100%',
-
-      marginBottom: '2.4rem',
-
-      backgroundColor: vars.colors.gray10,
-      color: vars.colors.white,
-      ...vars.fonts.b2_m,
-    },
-  });
+  toast(
+    () => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: icon ? '0.8em' : '0',
+        }}>
+        {icon && <IcClearMain />}
+        <span>{message}</span>
+      </div>
+    ),
+    {
+      duration: 1500,
+      position: 'bottom-center',
+      style: {
+        width: '100%',
+        marginBottom: '2.4rem',
+        backgroundColor: vars.colors.gray10,
+        color: vars.colors.white,
+        ...vars.fonts.b2_m,
+      },
+    }
+  );
 };
