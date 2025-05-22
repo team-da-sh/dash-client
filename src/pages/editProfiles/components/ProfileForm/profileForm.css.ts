a84844747693ab3bd8f3057a08eeeda68715248f
heesunee <heesun729@uos.ac.kr>
2025-05-22 18:25:48 +0900

    refactor: 확인버튼 맨 아래 (#442)

--- src/pages/editProfiles/components/ProfileForm/profileForm.css.ts
+++ src/pages/editProfiles/components/ProfileForm/profileForm.css.ts
@@ -1,10 +1,17 @@
 import { style } from '@vanilla-extract/css';
 
-export const buttonWrapperStyle = style({
-  padding: '2.4rem 0',
+export const formWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh',
+  justifyContent: 'space-between',
 });
 
 export const imageWrapperStyle = style({
   display: 'flex',
   justifyContent: 'center',
+});
+
+export const buttonWrapperStyle = style({
+  padding: '2.4rem 0',
 });
