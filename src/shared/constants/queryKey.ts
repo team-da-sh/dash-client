77b5d2ce6af03bf3c433c14f3bd86080fe9f7c55
kimkyuhong <bobo3766@naver.com>
2025-05-23 17:44:04 +0900

    feat: 기존 쿼리키 삭제

--- src/shared/constants/queryKey.ts
+++ src/shared/constants/queryKey.ts
@@ -1,40 +1,3 @@
-// export const QUERY_KEYS = {
-//   AUTH_REISSUE: 'auth_reissue',
-
-//   MEMBERS_ME: 'members_me',
-//   MEMBERS_ME_THUMBNAILS: 'members_me_thumbnails',
-//   MEMBERS_RESERVATIONS: 'members_reservations',
-//   MEMBERS_RESERVATION_DETAIL: 'members_reservation_detail',
-//   MEMBERS_RESERVATION_STATISTICS: 'members_reservation_statistics',
-
-//   TEACHERS: 'teachers',
-//   TEACHERS_ME: 'teachers_me',
-//   TEACHER_DETAIL: 'teacher_detail',
-//   TEACHER_DETAIL_INTRODUCTION: 'teacher_detail_introduction',
-//   TEACHERS_SEARCH: 'teachers_search',
-
-//   LESSONS: 'lessons',
-//   LESSON_DETAIL: 'lesson_detail',
-//   LESSONS_LATEST: 'lessons_latest',
-//   LESSONS_POPULAR_GENRES: 'lessons_popular_genres',
-//   LESSONS_UPCOMING: 'lessons_upcoming',
-//   LESSONS_FAVORITES: 'lessons_favorites',
-//   LESSON_RESERVE_PROGRESS: 'lesson_reserve_progress',
-//   LESSON_RESERVATION: 'lesson_reservation',
-
-//   MY_PAGE_FAVORITES: 'my_page_favorites',
-//   MY_PAGE_LESSONS: 'my_page_lessons',
-//   MY_PAGE_LESSON_DETAIL: 'my_page_lesson_detail',
-
-//   ADVERTISEMENTS: 'advertisements',
-
-//   SEARCH_LESSONS: 'search_lessons',
-
-//   LOCATIONS: 'locations',
-
-//   AUTH_ROLE: 'auth_role',
-// };
-
 interface lessonSearchTypes {
   genre: string;
   level: string;
@@ -56,18 +19,18 @@ export const lessonKeys = {
     keyword,
   ],
   detail: (lessonId: number) => [...lessonKeys.all, lessonId],
-  reserve_progress: (lessonId: number) => [...lessonKeys.detail(lessonId), 'reserve-progress'],
   latest: () => [...lessonKeys.all, 'latest'],
   popular_genre: () => [...lessonKeys.all, 'popular'],
   upcoming: () => [...lessonKeys.all, 'upcoming'],
+  reserve_progress: (lessonId: number) => [...lessonKeys.detail(lessonId), 'reserve-progress'],
 };
 
 export const memberKeys = {
   all: ['members'] as const,
   me: () => [memberKeys.all, 'me'],
   lessons: () => [...memberKeys.me(), 'lessons'],
-  thumbnails: () => [...memberKeys.lessons(), 'thumbnails'],
   reservations: () => [...memberKeys.me(), 'reservations'],
+  thumbnails: () => [...memberKeys.lessons(), 'thumbnails'],
   reservations_detail: (reservationId: number) => [...memberKeys.reservations(), reservationId],
   statistics: () => [memberKeys.reservations, 'statistics'],
 };
