b560e8b074fb018da8fdda4a44f4112735c0bcef
heesunee <heesun729@uos.ac.kr>
2025-05-27 21:18:31 +0900

    code review: 타입 상단 선언 (#442)

--- src/pages/editProfiles/utils/inputUtils.ts
+++ src/pages/editProfiles/utils/inputUtils.ts
@@ -1,10 +1,11 @@
-export const allowOnlyNumberKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+import type { KeyboardEvent, ClipboardEvent } from 'react';
+
+export const allowOnlyNumberKey = (e: KeyboardEvent<HTMLInputElement>) => {
   const isNumber =
     (e.key >= '0' && e.key <= '9') || ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight', 'Tab'].includes(e.key);
   if (!isNumber) e.preventDefault();
 };
-
-export const allowOnlyNumberPaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+export const allowOnlyNumberPaste = (e: ClipboardEvent<HTMLInputElement>) => {
   const paste = e.clipboardData.getData('text');
   if (!/^\d+$/.test(paste)) e.preventDefault();
 };
