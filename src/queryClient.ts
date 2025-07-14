b160ad7dd605c19efc3c209e3ca438743651106a
kimkyuhong <bobo3766@naver.com>
2025-07-15 00:28:20 +0900

    feat: mutation 에러 처리 mutation cache로 통일화

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,4 +1,7 @@
-import { QueryClient } from '@tanstack/react-query';
+import { MutationCache, QueryClient } from '@tanstack/react-query';
+import { notify } from '@/shared/components/Toast/Toast';
+
+const DEFAULT_ERROR_MESSAGE = '요청에 실패했어요.';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -7,5 +10,13 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false, // 브라우저 창이 다시 focus를 하면 refetch되는 옵션 off
     },
   },
+  mutationCache: new MutationCache({
+    onError(error, vars, ctx, mutation) {
+      if (mutation.meta?.shouldShowToastMessage) {
+        notify(mutation.meta?.errorMessage || DEFAULT_ERROR_MESSAGE, true);
+      }
+    },
+  }),
 });
+
 export default queryClient;
