336ee8429acf16abd2173ad90b87d3727581816f
kimkyuhong <bobo3766@naver.com>
2025-07-17 01:05:39 +0900

    feat: react query가 ErrorBoundary로 에러 전파 가능하게 만듬

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -7,9 +7,10 @@ const DEFAULT_ERROR_MESSAGE = '요청에 실패했어요.';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      suspense: true,
+      throwOnError: true,
       staleTime: 1000 * 60 * 1, // 1분 후 stale 상태로 처리
       refetchOnWindowFocus: false, // 브라우저 창이 다시 focus를 하면 refetch되는 옵션 off
+      retry: 0,
     },
   },
   mutationCache: new MutationCache({
