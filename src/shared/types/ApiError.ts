bbc98fc032317c02b37aaf64334fe49d39db02f8
kimkyuhong <bobo3766@naver.com>
2025-07-20 19:18:32 +0900

    feat: status 추가

--- src/shared/types/ApiError.ts
+++ src/shared/types/ApiError.ts
@@ -2,6 +2,7 @@
 import type { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { HTTP_STATUS_CODE } from '@/shared/constants/api';
 
+// Error name status 별로 확장한 ApiError class
 export class ApiError<T = unknown> extends Error implements AxiosError<T> {
   config: InternalAxiosRequestConfig;
   code?: string;
@@ -28,6 +29,12 @@ export class ApiError<T = unknown> extends Error implements AxiosError<T> {
         break;
       case HTTP_STATUS_CODE.NOT_FOUND: // 404
         name += ': NOT_FOUND';
+        break;
+      case HTTP_STATUS_CODE.METHOD_NOT_ALLOWED: // 405
+        name += ': METHOD_NOT_ALLOWED';
+        break;
+      case HTTP_STATUS_CODE.CONFLICT: // 409
+        name += ': CONFLICT';
         break;
       case HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR: // 500
         name += ': INTERNAL_SERVER_ERROR';
