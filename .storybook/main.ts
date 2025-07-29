3f02688107efcdee456b4c7675b3803d05de5c6c
heesunee <heesun729@uos.ac.kr>
2025-07-29 14:10:25 +0900

    feat: storybook 기본 설정 추가 (#474)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,14 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/manager-api',
+    '@storybook/theming',
   ],
   framework: {
     name: '@storybook/react-vite',
