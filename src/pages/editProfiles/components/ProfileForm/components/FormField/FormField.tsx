6bc5cd2a07bf3bc1c4d9323beb2798c34c2a4398
heesunee <heesun729@uos.ac.kr>
2025-07-29 13:50:12 +0900

    feat: logo stories 추가 (#474)

--- src/pages/editProfiles/components/ProfileForm/components/FormField/FormField.tsx
+++ src/pages/editProfiles/components/ProfileForm/components/FormField/FormField.tsx
@@ -1,6 +1,8 @@
 import type { UseFormRegister } from 'react-hook-form';
-import * as styles from '@/pages/editProfiles/components/ProfileForm/components/FormField/formField.css';
+import * as styles from '@/pages/editProfiles/components/FormField/formField.css';
 import type { ProfileFormValues } from '@/pages/editProfiles/schema/profileSchema';
+import { allowOnlyNumberKey, allowOnlyNumberPaste } from '@/pages/editProfiles/utils/inputUtils';
+import { MAX_PHONENUMBER_LENGTH, MAX_NAME_LENGTH } from '@/pages/onboarding/constants';
 import Input from '@/shared/components/Input/Input';
 import Text from '@/shared/components/Text/Text';
 
@@ -12,6 +14,9 @@ interface FormFieldPropTypes {
   error?: { message?: string };
   readOnly?: boolean;
   validationMessage?: React.ReactNode;
+  isFocused?: boolean;
+  onFocus?: () => void;
+  onBlur?: () => void;
 }
 
 const FormField = ({
@@ -22,21 +27,36 @@ const FormField = ({
   error,
   readOnly = false,
   validationMessage,
+  onFocus,
+  onBlur,
+  isFocused,
 }: FormFieldPropTypes) => {
+  const isPhoneNumber = name === 'phoneNumber';
+
   return (
     <div className={styles.fieldWrapperStyle}>
       <label>
         <Text tag="b2_sb">{label}</Text>
       </label>
-      <Input placeholder={placeholder} {...register(name)} isError={!!error} readOnly={readOnly} />
-      {error && error.message && (
-        <div className={styles.errorMessageStyle}>
+      <Input
+        placeholder={placeholder}
+        {...register(name)}
+        isError={!!error}
+        maxLength={name === 'phoneNumber' ? MAX_PHONENUMBER_LENGTH : MAX_NAME_LENGTH}
+        readOnly={readOnly}
+        isFocused={isFocused}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        {...(isPhoneNumber && { inputMode: 'numeric', onKeyDown: allowOnlyNumberKey, onPaste: allowOnlyNumberPaste })}
+      />
+      <div className={styles.errorMessageStyle({ hasError: !!(error && error.message) })}>
+        {error?.message && (
           <Text tag="b3_r" color="alert3">
             {error.message}
           </Text>
-          {validationMessage}
-        </div>
-      )}
+        )}
+        {validationMessage}
+      </div>
     </div>
   );
 };
---
+++ src/stories/foundations/logo/Logo.css.ts
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '3rem',
+});
+
+export const logoWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '2rem',
+  gap: '2rem',
+});
+
