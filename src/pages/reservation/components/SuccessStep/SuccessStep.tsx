a7dbd7b6ecf76ff951de9b37cd05bb2fa631599c
hansoojeongsj <tnwjd3758@gmail.com>
2025-08-07 03:20:18 +0900

    feat: 성공스탭 퍼블리싱 (#491)

--- src/pages/reservation/components/SuccessStep/SuccessStep.tsx
+++ src/pages/reservation/components/SuccessStep/SuccessStep.tsx
@@ -1,11 +1,31 @@
 import BoxButton from '@/shared/components/BoxButton/BoxButton';
+import Completion from '@/shared/components/Completion/Completion';
+import { sprinkles } from '@/shared/styles/sprinkles.css';
+import * as styles from './successStep.css';
 
 const SuccessStep = ({ onGoHome }: { onGoHome: () => void }) => {
   return (
-    <>
-      <div>SuccessStep</div>
-      <BoxButton onClick={onGoHome}>신청내역 보기</BoxButton>
-    </>
+    <div className={`${styles.flexCustomStyle}`}>
+      <div className={sprinkles({ flexDirection: 'column', width: '100%' })}>
+        <div
+          className={sprinkles({
+            display: 'flex',
+            flexDirection: 'column',
+            pl: 20,
+            pr: 20,
+            width: '100%',
+          })}>
+          <Completion
+            title="이제 거의 다 왔어요!"
+            subTitle="함께 리듬 탈 준비 됐나요?"
+            description="강사가 입금 확인 후 신청이 확정될 예정이에요"
+          />
+        </div>
+        <div className={styles.successButtonContainer}>
+          <BoxButton onClick={onGoHome}>신청내역으로 이동</BoxButton>
+        </div>
+      </div>
+    </div>
   );
 };
 
