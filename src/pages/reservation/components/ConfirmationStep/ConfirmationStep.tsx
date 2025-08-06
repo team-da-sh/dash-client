bb8854799914c4db3282732a7eda6567e1e62de5
hansoojeongsj <tnwjd3758@gmail.com>
2025-08-07 04:06:14 +0900

    feat: bottomsheet 제외 confirmstep 퍼블리싱 (#491)

--- src/pages/reservation/components/ConfirmationStep/ConfirmationStep.tsx
+++ src/pages/reservation/components/ConfirmationStep/ConfirmationStep.tsx
@@ -1,11 +1,86 @@
+import SvgIcCopy from '@/shared/assets/svg/IcCopy';
 import BoxButton from '@/shared/components/BoxButton/BoxButton';
+import Head from '@/shared/components/Head/Head';
+import Text from '@/shared/components/Text/Text';
+import { sprinkles } from '@/shared/styles/sprinkles.css';
+import * as styles from './confirmationStep.css';
 
-const ConfirmationStep = ({ onNext }: { onNext: () => void }) => {
+type ConfirmProps = {
+  onNext: () => void;
+  accountOwner?: string;
+  bank?: string;
+  accountNumber?: string;
+  price?: string;
+};
+
+const ConfirmationStep = ({
+  onNext,
+  accountOwner = '주은혜',
+  bank = '신한',
+  accountNumber = '4879899192818',
+  price = '350,000원',
+}: ConfirmProps) => {
+  const fullAccount = `${bank} ${accountNumber}`;
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(fullAccount);
+  };
+
   return (
-    <>
-      <div>ConfirmationStep</div>
-      <BoxButton onClick={onNext}>결제하기</BoxButton>
-    </>
+    <main>
+      <div className={styles.mainContainer}>
+        <Head level="h2" tag="h3_sb" color="black">
+          입금안내
+        </Head>
+
+        <Text tag="b2_m" color="gray7" className={styles.gapBetweenHeadAndNote}>
+          신청자명과 동일한 이름으로 입금해 주세요
+        </Text>
+
+        <Head level="h5" tag="h6_sb" color="black" className={styles.gapBeforeSection}>
+          입금정보
+        </Head>
+
+        <section className={styles.confirmSection}>
+          <div className={styles.rowItem}>
+            <Text tag="b2_m_long" color="gray6" className={styles.rowItemTitle}>
+              예금주
+            </Text>
+            <Text tag="b2_m_long" color="black">
+              {accountOwner}
+            </Text>
+          </div>
+          <div className={styles.rowItem}>
+            <Text tag="b2_m_long" color="gray6" className={styles.rowItemTitle}>
+              계좌번호
+            </Text>
+            <div className={sprinkles({ display: 'flex', gap: 6, alignItems: 'center' })}>
+              <Text tag="b2_m_long" color="black">
+                {bank}
+              </Text>
+              <Text tag="b2_m_long" color="black">
+                {accountNumber}
+              </Text>
+              <button onClick={handleCopy}>
+                <SvgIcCopy width={16} height={16} />
+              </button>
+            </div>
+          </div>
+          <div className={styles.rowItem}>
+            <Text tag="b2_m_long" color="gray6" className={styles.rowItemTitle}>
+              수강료
+            </Text>
+            <Text tag="b2_m_long" color="black">
+              {price}
+            </Text>
+          </div>
+        </section>
+      </div>
+
+      <div className={styles.confirmButtonContainer}>
+        <BoxButton onClick={onNext}>결제하기</BoxButton>
+      </div>
+    </main>
   );
 };
 
