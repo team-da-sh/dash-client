9ed03cb56952adbbb47fb98253076ac1a9ab5a4d
rtttr1 <bobo3766@naver.com>
2025-08-07 23:50:54 +0900

    feat: modal story 작성

---
+++ src/stories/Modal.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import type { ReactElement } from 'react';
+import Modal from '@/common/components/Modal/Modal';
+import useModal from '@/common/hooks/useModal';
+import BoxButton from '@/shared/components/BoxButton/BoxButton';
+
+const meta = {
+  title: 'Common/Modal',
+  component: Modal,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    content: {
+      control: {
+        type: 'text',
+      },
+      description: '모달의 설명을 적어주세요.',
+    },
+    type: {
+      control: {
+        type: 'radio',
+      },
+      options: ['default', 'single'],
+      description: '모달의 유형을 선택해주세요.',
+    },
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const ModalWithHooks = ({ type, content }: { type: 'default' | 'single'; content: string | ReactElement }) => {
+  const { openModal, closeModal } = useModal(() => (
+    <Modal type={type} content={content} onClose={closeModal} onClickHandler={open} />
+  ));
+
+  return (
+    <BoxButton variant="primary" onClick={openModal} style={{ width: '10rem' }}>
+      모달 오픈
+    </BoxButton>
+  );
+};
+
+export const Primary: Story = {
+  args: {
+    type: 'default',
+    content: '취소하겠습니다.',
+    onClose: () => {},
+  },
+  render: (args) => (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '37.5rem', height: '50rem' }}>
+      <ModalWithHooks {...args} />
+    </div>
+  ),
+};
