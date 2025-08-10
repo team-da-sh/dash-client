aac9a9d76c1b2a7b333529dd6a4e44152ff28435
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-08-11 00:27:30 +0900

    feat: CancelImformationConfirm 컴포넌트에 BoxButton 배치 (#499)

--- src/pages/mypage/components/mypageCancelClass/CancelImformationConfrim/CancelImformationConfirm.tsx
+++ src/pages/mypage/components/mypageCancelClass/CancelImformationConfrim/CancelImformationConfirm.tsx
@@ -1,5 +1,27 @@
+import BoxButton from '@/shared/components/BoxButton/BoxButton';
+import { sprinkles } from '@/shared/styles/sprinkles.css';
+import MyPageReservationDetail from '../../mypageReservationDetail/MypageReservationDetail';
+
 const CancelImformationConfirm = () => {
-  return <div>CancelImformationConfirm</div>;
+  const handleConfirm = () => {
+    window.location.href = '/mypage';
+  };
+
+  return (
+    <div
+      className={sprinkles({
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        justifyContent: 'space-between',
+      })}>
+      <MyPageReservationDetail />
+
+      <BoxButton variant="primary" onClick={handleConfirm}>
+        취소하기
+      </BoxButton>
+    </div>
+  );
 };
 
 export default CancelImformationConfirm;
