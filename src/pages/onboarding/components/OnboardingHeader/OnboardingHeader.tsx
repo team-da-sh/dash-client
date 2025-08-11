8b602f94b19d672b190a9f890abe33dc9a6a07e5
heesunee <heesun729@uos.ac.kr>
2025-08-11 22:13:49 +0900

    feat: 온보딩 헤더 스타일 수정 (#483)

--- src/pages/onboarding/components/OnboardingHeader/OnboardingHeader.tsx
+++ src/pages/onboarding/components/OnboardingHeader/OnboardingHeader.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import * as styles from '@/pages/onboarding/components/OnboardingHeader/onboardingHeaderStyle.css';
 import { ROUTES_CONFIG } from '@/routes/routesConfig';
+import IcBackBlack from '@/shared/assets/svg/IcBack';
 import IcHeaderLogoSmallBlack from '@/shared/assets/svg/IcHeaderLogoSmallBlack';
 
 const OnboardingHeader = () => {
@@ -10,10 +11,17 @@ const OnboardingHeader = () => {
     navigate(ROUTES_CONFIG.home.path);
   };
 
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <header className={styles.onboardingHeaderStyle}>
+      <button onClick={handleBackClick} aria-label="뒤로 이동">
+        <IcBackBlack width={24} height={24} className={styles.svgStyle} />
+      </button>
       <button onClick={handleLogoClick} aria-label="홈으로 이동">
-        <IcHeaderLogoSmallBlack width={58} height={20} />
+        <IcHeaderLogoSmallBlack width={58} height={20} className={styles.svgStyle} />
       </button>
     </header>
   );
