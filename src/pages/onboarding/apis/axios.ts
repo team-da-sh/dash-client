778b31c42da03ff1a4e31d9014cbff1badc8e771
heesunee <heesun729@uos.ac.kr>
2025-08-11 18:07:53 +0900

    fix: URL 엔드포인트 상수로 수정 (#483)

--- src/pages/onboarding/apis/axios.ts
+++ src/pages/onboarding/apis/axios.ts
@@ -1,10 +1,11 @@
 import type { tokenTypes } from '@/pages/onboarding/types/api';
-import type { onboardInfoTypes } from '@/pages/onboarding/types/onboardInfoTypes';
+import type { OnboardInfoTypes } from '@/pages/onboarding/types/onboardInfoTypes';
 import { instance } from '@/shared/apis/instance';
+import { API_URL } from '@/shared/constants/apiURL';
 
-export const postOnboard = async ({ name, phoneNumber, accessToken }: onboardInfoTypes & tokenTypes) => {
+export const postOnboard = async ({ name, phoneNumber, accessToken }: OnboardInfoTypes & tokenTypes) => {
   const response = await instance.post(
-    `/api/v1/members/onboard`,
+    API_URL.MEMBERS_ONBOARD,
     {
       name,
       phoneNumber,
