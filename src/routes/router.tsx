1389b8fedab03c1ba6f81b67d9112cfaab8eaa90
heesunee <heesun729@uos.ac.kr>
2025-08-19 21:12:18 +0900

    feat: onboardingguard 추가 (#503)

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -6,8 +6,11 @@ import { guestRoutes } from '@/routes/sections/guestRoutes';
 import { protectedRoutes } from '@/routes/sections/protectedRoutes';
 import { publicRoutes } from '@/routes/sections/publicRoutes';
 import Layout from '@/layout/Layout';
+import OnboardingGuard from './guards/onboardingGuard';
+import { ROUTES_CONFIG } from './routesConfig';
 
 const Error = lazy(() => import('@/pages/error/Error'));
+const Onboarding = lazy(() => import('@/pages/onboarding/Onboarding'));
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +21,7 @@ export const router = createBrowserRouter([
         element: <GuestGuard />,
         children: [...guestRoutes],
       },
+      { element: <OnboardingGuard />, children: [{ path: ROUTES_CONFIG.onboarding.path, element: <Onboarding /> }] },
 
       ...publicRoutes,
 
