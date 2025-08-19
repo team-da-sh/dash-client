d09e6f388cf674429dc586a70fe40911d474046c
heesunee <heesun729@uos.ac.kr>
2025-08-19 21:23:56 +0900

    feat: home 경로 추가 및 폴더 이름 변경 (#503)

--- src/routes/modules/publicRoutes.tsx
+++ src/routes/modules/publicRoutes.tsx
@@ -1,4 +1,5 @@
 import { lazy } from 'react';
+import Home from '@/pages/home/Home';
 import { ROUTES_CONFIG } from '@/routes/routesConfig';
 
 const Search = lazy(() => import('@/pages/search/Search'));
@@ -6,6 +7,7 @@ const Class = lazy(() => import('@/pages/class/Class'));
 const Dancer = lazy(() => import('@/pages/dancer/Dancer'));
 
 export const publicRoutes = [
+  { path: ROUTES_CONFIG.home.path, element: <Home /> },
   { path: ROUTES_CONFIG.search.path, element: <Search /> },
   { path: ROUTES_CONFIG.class.path(':id'), element: <Class /> },
   { path: ROUTES_CONFIG.dancer.path(':id'), element: <Dancer /> },
