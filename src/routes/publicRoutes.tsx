7ead4bc56a4e1ace7e0a53847e58c4b1982e3f19
heesunee <heesun729@uos.ac.kr>
2025-08-19 20:06:04 +0900

    feat: add public routes (#503)

---
+++ src/routes/publicRoutes.tsx
@@ -0,0 +1,12 @@
+import { ROUTES_CONFIG } from './routesConfig';
+
+export const publicRoutes = [
+  ROUTES_CONFIG.home.path,
+  ROUTES_CONFIG.login.path,
+  ROUTES_CONFIG.auth.path,
+  ROUTES_CONFIG.onboarding.path,
+  ROUTES_CONFIG.search.path,
+  ROUTES_CONFIG.class.path(':id'),
+  ROUTES_CONFIG.dancer.path(':id'),
+  ROUTES_CONFIG.error.path,
+];
