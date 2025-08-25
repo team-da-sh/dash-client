d8aa4f351901cb2c0beb2e18b4c523173d6480a0
김건휘 <dirnrlarjsgn@naver.com>
2025-08-25 23:54:52 +0900

    fix: 런타임 에러 해결

--- src/pages/mypage/components/mypageCancelClass/MypageCancelClass.tsx
+++ src/pages/mypage/components/mypageCancelClass/MypageCancelClass.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import Error from '@/pages/error/Error';
 import * as styles from '@/pages/mypage/components/mypageCancelClass/mypageCancelClass.css';
 import { useFunnel } from '@/shared/hooks/useFunnel';
 import type { Reservation } from '@/shared/types/reservationTypes';
@@ -12,11 +11,7 @@ const MypageCancelClass = () => {
   const [selectedStatus, setSelectedStatus] = useState<'before' | 'after' | null>(null);
   const { Funnel, Step, setStep } = useFunnel(2, '/mypage', false);
 
-  const reservationFromState = location.state?.reservation as Reservation | undefined;
-
-  if (!reservationFromState) {
-    return <Error />;
-  }
+  const reservationFromState = location.state?.reservation as Reservation;
 
   const reservationData = reservationFromState;
 
