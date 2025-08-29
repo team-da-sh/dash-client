4bfa722c100e4825654f83a39bdca2608e257f29
rtttr1 <bobo3766@naver.com>
2025-08-29 22:10:08 +0900

    refactor: 클래스 카드 합성 컴포넌트 방식으로 수정

--- src/pages/mypage/components/mypageReservation/components/ReservationList.tsx
+++ src/pages/mypage/components/mypageReservation/components/ReservationList.tsx
@@ -4,7 +4,7 @@ import { type ReservationStatus } from '@/pages/mypage/components/mypageReservat
 import { type Reservation } from '@/pages/mypage/components/mypageReservation/types/reservationTypes';
 import { handleBoxButtonClick, handleCancelClick, handleClassCardClick } from '@/pages/mypage/utils/clickUtils';
 import BoxButton from '@/shared/components/BoxButton/BoxButton';
-import ClassCard from '@/shared/components/ClassCard/ClassCard';
+import ClassCard from '@/shared/components/ClassCard';
 
 interface ReservationListProps {
   status: ReservationStatus;
@@ -21,27 +21,32 @@ const ReservationList = ({ status }: ReservationListProps) => {
       {reservationData?.reservations.map((reservation: Reservation) => (
         <ClassCard
           key={reservation.reservationId}
-          id={reservation.reservationId}
-          name={reservation.name}
-          imageUrl={reservation.imageUrl}
-          genre={reservation.genre}
-          level={reservation.level}
-          location={reservation.location}
-          detailedAddress={reservation.location}
-          startDateTime={reservation.startDateTime}
-          endDateTime={reservation.endDateTime}
-          isReservation={true}
-          applyStatus={reservation.attendStatus}
           onClick={() => handleClassCardClick(navigate, reservation.reservationId)}>
-          <BoxButton onClick={handleCancelClick} variant="temp">
-            취소하기
-          </BoxButton>
+          <ClassCard.Header
+            role="student"
+            status={reservation.reservationStatus}
+            date={reservation.reservationDateTime}
+          />
+          <ClassCard.Body
+            name={reservation.name}
+            imageUrl={reservation.imageUrl}
+            genre={reservation.genre}
+            level={reservation.level}
+            location={reservation.location}
+            startDateTime={reservation.startDateTime}
+            endDateTime={reservation.endDateTime}
+          />
+          <ClassCard.Footer showAsk={true}>
+            <BoxButton onClick={handleCancelClick} variant="temp">
+              취소하기
+            </BoxButton>
 
-          <BoxButton
-            variant="outline"
-            onClick={(e) => handleBoxButtonClick(e, navigate, reservation.reservationId, true)}>
-            상세보기
-          </BoxButton>
+            <BoxButton
+              variant="outline"
+              onClick={(e) => handleBoxButtonClick(e, navigate, reservation.reservationId, true)}>
+              상세보기
+            </BoxButton>
+          </ClassCard.Footer>
         </ClassCard>
       ))}
     </>
---
+++ src/shared/components/ClassCard/ClassCardHeader.tsx
@@ -0,0 +1,76 @@
+import type { ReservationStatus } from '@/pages/mypage/components/mypageReservation/types/reservationStatus';
+import SvgIcClearAlert20 from '@/shared/assets/svg/IcClearAlert20';
+import SvgIcClearMain0320 from '@/shared/assets/svg/IcClearMain0320';
+import SvgIcMeatballAlert20 from '@/shared/assets/svg/IcMeatballAlert20';
+import SvgIcMeatballMain0320 from '@/shared/assets/svg/IcMeatballMain0320';
+import * as styles from '@/shared/components/ClassCard/style.css';
+import Divider from '@/shared/components/Divider/Divider';
+import Text from '@/shared/components/Text/Text';
+import { sprinkles } from '@/shared/styles/sprinkles.css';
+import { formatDate } from '@/shared/utils/timeCalculate';
+
+const studentStatus = (status: ReservationStatus) => {
+  switch (status) {
+    case 'PENDING_APPROVAL':
+      return [<SvgIcMeatballMain0320 width={20} height={20} />, '승인 대기'];
+    case 'APPROVED':
+      return [<SvgIcClearMain0320 width={20} height={20} />, '승인 완료'];
+    case 'PENDING_CANCELLATION':
+      return [<SvgIcMeatballAlert20 width={20} height={20} />, '취소 대기'];
+    case 'CANCELLED':
+      return [<SvgIcClearAlert20 width={20} height={20} />, '취소 완료'];
+    default:
+      return [null, '상태 불명'];
+  }
+};
+
+const teacherStatus = (status: string) => {
+  switch (status) {
+    case 'recruiting':
+      return [<SvgIcMeatballMain0320 width={20} />, '모집 중'];
+    case 'recruitingEnd':
+      return [<SvgIcClearMain0320 width={20} />, '모집 완료'];
+    default:
+      return [null, '상태 불명'];
+  }
+};
+
+interface ClassCardHeaderProps {
+  role: 'teacher' | 'student';
+  status: ReservationStatus;
+  date: string;
+}
+
+const ClassCardHeader = ({ role, status, date }: ClassCardHeaderProps) => {
+  const [statusIcon, statusText] = role === 'student' ? studentStatus(status) : teacherStatus(status);
+  console.log(statusIcon);
+  return (
+    <>
+      <div
+        className={sprinkles({
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        })}>
+        <div
+          className={sprinkles({
+            display: 'flex',
+            alignItems: 'center',
+            gap: 8,
+          })}>
+          {/* <div className={sprinkles({ marginRight: 5, display: 'flex', alignItems: 'center' })}>{statusIcon}</div> */}
+          {statusIcon}
+          <Text tag="b1_sb">{statusText}</Text>
+          {date && (
+            <Text tag="b3_r" color="gray7">
+              {`${formatDate(date)} ${role === 'student' ? '신청' : '개설'}`}
+            </Text>
+          )}
+        </div>
+      </div>
+      <Divider color="gray1" className={styles.dividerStyle} />
+    </>
+  );
+};
+
+export default ClassCardHeader;
