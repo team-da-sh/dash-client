d799f93f69c64cf98a7ae5461de9059702d94e96
rtttr1 <bobo3766@naver.com>
2025-09-05 00:52:04 +0900

    feat: lessonDetail get mock api 구현

--- src/mocks/handler/index.ts
+++ src/mocks/handler/index.ts
@@ -1,4 +1,5 @@
 import { classRegisterHandlers } from '@/mocks/handler/classRegister';
 import { homeHandlers } from '@/mocks/handler/home';
+import { instructorHandlers } from '@/mocks/handler/instructor';
 
-export const handlers = [...homeHandlers, ...classRegisterHandlers];
+export const handlers = [...homeHandlers, ...classRegisterHandlers, ...instructorHandlers];
