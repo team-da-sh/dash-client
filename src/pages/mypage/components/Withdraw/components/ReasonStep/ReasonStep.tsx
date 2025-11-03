a7a3551ab9f3274f299ed57f849914ef68bea433
hansoojeongsj <tnwjd3758@gmail.com>
2025-11-03 13:43:54 +0900

    feat: IcCheckcircleGray 아이콘 색 변경 가능하게 수정 (#579)

--- src/pages/mypage/components/Withdraw/components/ReasonStep/ReasonStep.tsx
+++ src/pages/mypage/components/Withdraw/components/ReasonStep/ReasonStep.tsx
@@ -1,4 +1,6 @@
+import IcCheckcircleGray0524 from '@/shared/assets/svg/IcCheckcircleGray0524';
 import BoxButton from '@/shared/components/BoxButton/BoxButton';
+import { vars } from '@/shared/styles/theme.css';
 
 interface ReasonStepPropTypes {
   onNext: () => void;
@@ -8,6 +10,7 @@ const ReasonStep = ({ onNext }: ReasonStepPropTypes) => {
   return (
     <>
       reasonStepPage
+      <IcCheckcircleGray0524 width={24} height={24} color={vars.colors.gray03} />
       <BoxButton onClick={onNext}>확인</BoxButton>
     </>
   );
