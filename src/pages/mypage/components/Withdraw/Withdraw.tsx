6ca87411e61cfdc71baf693c38d30e765efc96fd
hansoojeongsj <tnwjd3758@gmail.com>
2025-11-03 13:22:42 +0900

    feat: 탈퇴 뷰 헤더 추가 (#579)

--- src/pages/mypage/components/Withdraw/Withdraw.tsx
+++ src/pages/mypage/components/Withdraw/Withdraw.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import CompleteStep from '@/pages/mypage/components/Withdraw/components/CompleteStep/CompleteStep';
 import NoticeStep from '@/pages/mypage/components/Withdraw/components/NoticeStep/NoticeStep';
 import ReasonStep from '@/pages/mypage/components/Withdraw/components/ReasonStep/ReasonStep';
+import WithdrawHeader from '@/pages/mypage/components/Withdraw/components/WithdrawHeader/WithdrawHeader';
 import { ROUTES_CONFIG } from '@/routes/routesConfig';
 import { useFunnel } from '@/shared/hooks/useFunnel';
 
@@ -12,17 +13,17 @@ const Withdraw = () => {
   return (
     <Funnel>
       <Step name="1">
-        {/* <Header step={1} /> */}
+        <WithdrawHeader step={1} />
         <ReasonStep onNext={() => setStep(1)} />
       </Step>
 
       <Step name="2">
-        {/* <Header step={2} /> */}
+        <WithdrawHeader step={2} />
         <NoticeStep onNext={() => setStep(1)} />
       </Step>
 
       <Step name="3">
-        {/* <Header step={3} /> */}
+        <WithdrawHeader step={3} />
         <CompleteStep onGoHome={() => navigate(ROUTES_CONFIG.home.path)} />
       </Step>
     </Funnel>
