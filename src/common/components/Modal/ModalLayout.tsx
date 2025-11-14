e21aec6395b34c4ddc4f56e3184d8c0a49a92b6c
rtttr1 <bobo3766@naver.com>
2025-11-14 18:54:16 +0900

    feat: esc 키로 모달 닫기 지원

--- src/common/components/Modal/ModalLayout.tsx
+++ src/common/components/Modal/ModalLayout.tsx
@@ -1,4 +1,4 @@
-import type { PropsWithChildren } from 'react';
+import { useEffect, type PropsWithChildren } from 'react';
 import { layoutStyle } from '@/common/components/Modal/modal.css';
 
 interface ModalLayoutProps extends PropsWithChildren {
@@ -6,6 +6,17 @@ interface ModalLayoutProps extends PropsWithChildren {
 }
 
 const ModalLayout = ({ onClose, children }: ModalLayoutProps) => {
+  // esc 키 누르면 모달 닫기
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [onClose]);
+
   return (
     <div
       onClick={(e) => {
