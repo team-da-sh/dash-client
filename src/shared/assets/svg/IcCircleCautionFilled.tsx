c5339eeb010dfddac5c82b47e180d386c85dc43d
hansoojeongsj <tnwjd3758@gmail.com>
2025-11-24 21:58:38 +0900

    chore: ic_circle_caution_filled svg 추가 (#610)

---
+++ src/shared/assets/svg/IcCircleCautionFilled.tsx
@@ -0,0 +1,9 @@
+import type { SVGProps } from 'react';
+
+const SvgIcCircleCautionFilled = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 54 54" {...props}>
+    <rect width={54} height={54} fill="#E5E7EB" rx={27} />
+    <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={5.4} d="M27 29.7V15.215M27 37.8z" />
+  </svg>
+);
+export default SvgIcCircleCautionFilled;
