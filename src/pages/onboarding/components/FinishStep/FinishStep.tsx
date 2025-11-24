3437269d256b9d02619d3ff476d559d40ffc2007
hansoojeongsj <tnwjd3758@gmail.com>
2025-11-24 21:52:24 +0900

    feat: 탈퇴 후 재가입 시, finishStep에서 글 변경 (#610)

--- src/pages/onboarding/components/FinishStep/FinishStep.tsx
+++ src/pages/onboarding/components/FinishStep/FinishStep.tsx
@@ -5,17 +5,30 @@ import Text from '@/shared/components/Text/Text';
 
 interface FinishStepProps {
   name: string;
+  isDeleted?: boolean;
 }
 
-const FinishStep = ({ name }: FinishStepProps) => {
+const FinishStep = ({ name, isDeleted }: FinishStepProps) => {
+  const title = isDeleted ? `${name} 님\n돌아오신 걸 환영해요!` : `환영해요 ${name} 님\n이제 몸을 풀어볼까요?`;
+
+  const description = isDeleted
+    ? '회원님의 이전 활동 내역이 완벽하게 복구되었어요'
+    : '취향에 꼭 맞는 댄스 클래스를 둘러보고 신청할 수 있어요';
+
   return (
     <div className={styles.containerStyle}>
       <div className={styles.wrapperStyle}>
         <Head level="h1" tag="h3_sb">
-          환영해요 {name} 님 <br /> 이제 몸을 풀어볼까요?
+          {title.split('\n').map((line) => (
+            <span key={line}>
+              {line}
+              <br />
+            </span>
+          ))}
         </Head>
+
         <Text tag="b2_m" color="gray7">
-          취향에 꼭 맞는 댄스 클래스를 둘러보고 신청할 수 있어요
+          {description}
         </Text>
       </div>
 
